72f5aecb731a77da8b09ceecb99c310dc18123c7
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2007-10-09 23:10:32 +0000

    Release version 0.5.61

--- smplayer/trunk/src/version.cpp
+++ smplayer/trunk/src/version.cpp
@@ -18,9 +18,9 @@
 
 #include "version.h"
 
-#define USE_SVN_VERSIONS 1
+//#define USE_SVN_VERSIONS 1
 
-#define VERSION "0.5.60"
+#define VERSION "0.5.61"
 
 #if USE_SVN_VERSIONS
 #include "svn_revision.h"
