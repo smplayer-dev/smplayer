23a38c5370355ae00462efa45a7b5ff869cf53fb
henrikx <henrikx@fd4b1137-b938-0410-b384-8fdad0efb714>
2007-11-13 10:57:26 +0000

    Übersetzung rev 404

--- smplayer/trunk/src/translations/smplayer_de.ts
+++ smplayer/trunk/src/translations/smplayer_de.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="de">
+<defaultcodec></defaultcodec>
 <context>
     <name>AboutDialog</name>
     <message>
@@ -3092,7 +3093,7 @@ For translators: don&apos;t translate this text, it will be replaced with anothe
     <message>
         <location filename="../prefinput.cpp" line="100"/>
         <source>Change speed</source>
-        <translation type="unfinished"></translation>
+        <translation>Geschwindigkeit ändern</translation>
     </message>
 </context>
 <context>
