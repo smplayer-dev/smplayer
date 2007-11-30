a3c5f5fc924d547c1fca82f6f83a331e5aeacc77
DoruHush <DoruHush@fd4b1137-b938-0410-b384-8fdad0efb714>
2007-11-30 11:38:21 +0000

    "Updated the Romanian Translation"

--- src/translations/smplayer_ro_RO.ts
+++ src/translations/smplayer_ro_RO.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="en_US">
+<defaultcodec></defaultcodec>
 <context>
     <name>AboutDialog</name>
     <message>
@@ -3629,57 +3630,57 @@ For translators: don&apos;t translate this text, it will be replaced with anothe
     <message>
         <location filename="../prefperformance.cpp" line="261"/>
         <source>Cache for files</source>
-        <translation type="unfinished"></translation>
+        <translation>Cache pentru fişiere</translation>
     </message>
     <message>
         <location filename="../prefperformance.cpp" line="263"/>
         <source>This option specifies how much memory (in kBytes) to use when precaching a file.</source>
-        <translation type="unfinished"></translation>
+        <translation>Această opţiune stabileşte câtă memorie (în kBytes) se va folosi la încărcarea unui fişier în cache.</translation>
     </message>
     <message>
         <location filename="../prefperformance.cpp" line="265"/>
         <source>Cache for streams</source>
-        <translation type="unfinished"></translation>
+        <translation>Cache pentru URL</translation>
     </message>
     <message>
         <location filename="../prefperformance.cpp" line="267"/>
         <source>This option specifies how much memory (in kBytes) to use when precaching a URL.</source>
-        <translation type="unfinished"></translation>
+        <translation>Această opţiune stabileşte câtă memorie (în kBytes) se va folosi pentru cache la redarea unui fişier video de pe internet.</translation>
     </message>
     <message>
         <location filename="../prefperformance.cpp" line="269"/>
         <source>Cache for DVDs</source>
-        <translation type="unfinished"></translation>
+        <translation>Cache pentru DVD</translation>
     </message>
     <message>
         <location filename="../prefperformance.cpp" line="272"/>
         <source>This option specifies how much memory (in kBytes) to use when precaching a DVD.&lt;br&gt;&lt;b&gt;Warning:&lt;/b&gt; Seeking might not work properly (including chapter switching) when using a cache for DVDs.</source>
-        <translation type="unfinished"></translation>
+        <translation>Această opţiune stabileşte câtă memorie (în kBytes) se va folosii la încărcarea în cache a DVD-urilor.&lt;br&gt;&lt;b&gt;Atenţie:&lt;/b&gt;Este posibil ca funcţia de căutare să nu mai funcţioneze corect (inclusiv căutarea capitolelor) când se foloseşte încărcarea în cache a DVD-urilor.</translation>
     </message>
     <message>
         <location filename="../prefperformance.ui" line="312"/>
         <source>C&amp;reate index if needed</source>
-        <translation type="unfinished"></translation>
+        <translation>C&amp;reare index dacă este nevoie</translation>
     </message>
     <message>
         <location filename="../prefperformance.ui" line="333"/>
         <source>&amp;Cache</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Cache</translation>
     </message>
     <message>
         <location filename="../prefperformance.ui" line="379"/>
         <source>Cache for &amp;DVDs:</source>
-        <translation type="unfinished"></translation>
+        <translation>Cache pentru &amp;DVD:</translation>
     </message>
     <message>
         <location filename="../prefperformance.ui" line="399"/>
         <source>Cache for &amp;local files:</source>
-        <translation type="unfinished"></translation>
+        <translation>Cache pentru fişiere &amp;locale:</translation>
     </message>
     <message>
         <location filename="../prefperformance.ui" line="409"/>
         <source>Cache for &amp;streams:</source>
-        <translation type="unfinished"></translation>
+        <translation>Cache pentru &amp;streams (URL):</translation>
     </message>
 </context>
 <context>
