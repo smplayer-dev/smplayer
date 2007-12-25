fbe17dedf3b7df96b9bf3d374f8f718220d0a625
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2007-12-25 09:40:23 +0000

    Disable some code on Win98

--- smplayer/trunk/src/helper.cpp
+++ smplayer/trunk/src/helper.cpp
@@ -239,17 +239,22 @@ QString Helper::timeForJumps(int secs) {
 // This function has been copied (and modified a little bit) from Scribus (program under GPL license):
 // http://docs.scribus.net/devel/util_8cpp-source.html#l00112
 QString Helper::shortPathName(QString long_path) {
-	QString short_path = long_path;
+	if (QSysInfo::WindowsVersion >= QSysInfo::WV_NT) {
+		QString short_path = long_path;
 	
-	int max_path = 4096;
-	WCHAR shortName[max_path];
+		int max_path = 4096;
+		WCHAR shortName[max_path];
 
-	QString nativePath = QDir::convertSeparators(long_path);
-	int ret = GetShortPathNameW((LPCWSTR) nativePath.utf16(), shortName, max_path);
-	if (ret != ERROR_INVALID_PARAMETER && ret < MAX_PATH)
-		short_path = QString::fromUtf16((const ushort*) shortName);
+		QString nativePath = QDir::convertSeparators(long_path);
+		int ret = GetShortPathNameW((LPCWSTR) nativePath.utf16(), shortName, max_path);
+		if (ret != ERROR_INVALID_PARAMETER && ret < MAX_PATH)
+			short_path = QString::fromUtf16((const ushort*) shortName);
 
-	return short_path;
+		return short_path;
+	} else {
+		//Win98
+		return long_path;
+	}
 }
 
 #if !DISABLE_SCREENSAVER_BY_EVENT
