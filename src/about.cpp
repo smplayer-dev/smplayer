09996c6ebceb1aa61ab7446bf177fd4ec62fd2fd
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2008-01-17 00:31:05 +0000

    * Now all the gpl.html in docs/ should be in UTF-8.  * Converted docs/es/gpl.html and docs/de/gpl.html to UTF-8.  * Added docs/ru/gpl.html.

--- src/about.cpp
+++ src/about.cpp
@@ -66,7 +66,7 @@ About::About(QWidget * parent, Qt::WindowFlags f)
 
 		QFile f(license_file);
 		if (f.open(QIODevice::ReadOnly)) {
-			license->setText(f.readAll());
+			license->setText(QString::fromUtf8(f.readAll()));
 		}
 		f.close();
 	} else {
