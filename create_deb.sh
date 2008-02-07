5ae1636f71cb1bb9e0da37796e75ca1558fb3a01
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2008-02-07 10:40:03 +0000

    Now the script create_deb.sh should create the deb package much faster, as it compiles only the files that have changed

--- create_deb.sh
+++ create_deb.sh
@@ -6,7 +6,13 @@ cp debian-rvm/changelog-orig debian-rvm/changelog
 ./get_svn_revision.sh
 dch -v 0.6.0rc1-`cat svn_revision.txt` "New version"
 
-dpkg-buildpackage -rfakeroot
+#dpkg-buildpackage -rfakeroot
+
+# This should be faster:
+rm build-stamp
+rm src/smplayer
+fakeroot debian/rules build
+fakeroot debian/rules binary
 
 rm debian-rvm/changelog
 rm debian
