1ef0e14ff72e73b2043113466bf3506b4bd1878a
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2008-02-13 00:41:53 +0000

    Version changed to 0.6.0rc2

--- smplayer/trunk/src/version.cpp
+++ smplayer/trunk/src/version.cpp
@@ -18,9 +18,9 @@
 
 #include "version.h"
 
-#define USE_SVN_VERSIONS 1
+#define USE_SVN_VERSIONS 0
 
-#define VERSION "0.6.0rc1"
+#define VERSION "0.6.0rc2 (SVN 834)"
 
 #if USE_SVN_VERSIONS
 #include "svn_revision.h"
