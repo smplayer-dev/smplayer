ab43badff2b0684bff6a37d80b1be8f4c0aaf267
wiselord <wiselord@fd4b1137-b938-0410-b384-8fdad0efb714>
2008-03-03 19:50:10 +0000

    Updated Russian translation

--- smplayer/trunk/src/translations/smplayer_ru_RU.ts
+++ smplayer/trunk/src/translations/smplayer_ru_RU.ts
@@ -3689,7 +3689,7 @@ For translators: don&apos;t translate this text, it will be replaced with anothe
     <message>
         <location filename="../prefinterface.cpp" line="101"/>
         <source>Macedonian</source>
-        <translation type="unfinished"></translation>
+        <translation>Македонский</translation>
     </message>
 </context>
 <context>
