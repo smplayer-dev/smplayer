a41d536f73b0b77aaccde475cb2639a2091c605d
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2008-03-22 12:19:41 +0000

    Changed version to 0.6.0rc3

--- src/version.cpp
+++ src/version.cpp
@@ -18,9 +18,9 @@
 
 #include "version.h"
 
-#define USE_SVN_VERSIONS 1
+#define USE_SVN_VERSIONS 0
 
-#define VERSION "0.6.0rc2"
+#define VERSION "0.6.0rc3 (SVN r1000)"
 
 #if USE_SVN_VERSIONS
 #include "svn_revision.h"
