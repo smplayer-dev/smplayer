5bb2e4d31ab53ef38fc70a7109827b2ca0258b34
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2008-03-27 02:08:24 +0000

    Updated test app

--- src/testcorelib/test.cpp
+++ src/testcorelib/test.cpp
@@ -1,6 +1,22 @@
+/*  smplayer, GUI front-end for mplayer.
+    Copyright (C) 2006-2008 Ricardo Villalba <rvm@escomposlinux.org>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
 #include "test.h"
-#include "mplayerwindow.h"
 #include "smplayercorelib.h"
 #include <QAction>
 #include <QMenuBar>
@@ -13,10 +29,8 @@
 Gui::Gui( QWidget * parent, Qt::WindowFlags flags ) 
 	: QMainWindow(parent, flags)
 {
-	mpw = new MplayerWindow(this);
-	setCentralWidget(mpw);
-
-	smplayerlib = new SmplayerCoreLib(mpw, this);
+	smplayerlib = new SmplayerCoreLib(this);
+	setCentralWidget(smplayerlib->mplayerWindow());
 
 	QAction * openAct = new QAction( tr("&Open..."), this);
 	connect( openAct, SIGNAL(triggered()), this, SLOT(open()) );
@@ -43,10 +57,6 @@ Gui::Gui( QWidget * parent, Qt::WindowFlags flags )
 
 
 	TimeSlider * time_slider = new TimeSlider(this);
-	//time_slider->setOrientation(Qt::Horizontal);
-	//time_slider->setTracking(false);
-	//time_slider->setMinimum(0);
-	//time_slider->setMaximum(100);
 	connect( time_slider, SIGNAL(posChanged(int)), 
              smplayerlib->core(), SLOT(goToPos(int)) );
 	connect( smplayerlib->core(), SIGNAL(posChanged(int)),
