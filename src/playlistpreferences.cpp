400e222b9af1bce26bf9d14b23bb0ac6c71b13d1
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2008-04-21 08:41:41 +0000

    The playlist preferences dialog is simpler now

--- src/playlistpreferences.cpp
+++ src/playlistpreferences.cpp
@@ -23,8 +23,20 @@ PlaylistPreferences::PlaylistPreferences( QWidget* parent, Qt::WindowFlags f )
 {
     setupUi(this);
 	//setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
-	//layout()->setSizeConstraint(QLayout::SetFixedSize);
-	adjustSize();
+	layout()->setSizeConstraint(QLayout::SetFixedSize);
+	//adjustSize();
+
+	recursive_check->setWhatsThis(
+		"Check this option if you want that adding a directory will also add "
+        "the files in subdirectories recursively. Otherwise only the files in "
+        "the selected directory will be added.");
+
+	getinfo_check->setWhatsThis(
+		"Check this option to inquire the files to be added to the playlist "
+        "for some info. That allows to show the title name (if available) and "
+        "length of the files. Otherwise this info won't be available until "
+        "the file is actually played. Beware: this option can be slow, "
+        "specially if you add many files.");
 }
 
 PlaylistPreferences::~PlaylistPreferences() {
