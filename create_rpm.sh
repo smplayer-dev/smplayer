e88bdc48c891fc0e2b0b963182ca4a177451d11a
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2008-05-11 01:31:19 +0000

    Added the script create_rpm.sh made by iSage (http://smplayer.berlios.de/forums/viewtopic.php?pid=1887#p1887)

---
+++ create_rpm.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#svn up
+./get_svn_revision.sh
+
+SVN_REVISION=`cat svn_revision.txt | sed -e 's/SVN-/svn_/g'`
+SMPVERSION=`cat src/version.cpp | grep "#define VERSION " | sed -e 's/#define VERSION "//g' -e 's/"$//g'`
+
+
+svn export . /tmp/smplayer-${SMPVERSION}_${SVN_REVISION}
+CURDIR=`pwd`
+cd /tmp
+tar cvjf smplayer-${SMPVERSION}_${SVN_REVISION}.tar.bz2 smplayer-${SMPVERSION}_${SVN_REVISION}/
+rm -r /tmp/smplayer-${SMPVERSION}_${SVN_REVISION}
+cat ${CURDIR}/smplayer.spec | sed -e 's/%define version [a-zA-Z0-9\.]*$/%define version '${SMPVERSION}'_'${SVN_REVISION}'/' > /tmp/smplayer.spec
+PCKGDIR=/usr/src/packages/
+if [ -e /etc/fedora-release ]; then
+    PCKGDIR=/usr/source/redhat/
+fi
+if [ -e /etc/mandrake-release ]; then
+    PCKGDIR=/usr/source/rpm/
+fi
+cp /tmp/smplayer-${SMPVERSION}_${SVN_REVISION}.tar.bz2 ${PCKGDIR}
+rpmbuild -bb --clean --rmsource smplayer.spec
+
