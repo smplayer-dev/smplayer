f37d5c2a5dd5a71698a7c840047c9b7df1fdde17
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2008-05-30 23:54:33 +0000

    Displaying the svn revision in the about dialog again

--- src/version.cpp
+++ src/version.cpp
@@ -18,9 +18,9 @@
 
 #include "version.h"
 
-#define USE_SVN_VERSIONS 0
+#define USE_SVN_VERSIONS 1
 
-#define VERSION "0.6.1 (SVN r1304)"
+#define VERSION "0.6.1"
 
 #if USE_SVN_VERSIONS
 #include "svn_revision.h"
