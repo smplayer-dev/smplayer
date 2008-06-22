fb08fd11154ea7c1d1fcbe4ba825df51faee608a
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2008-06-22 00:05:57 +0000

    Fix: display languages with '-'

--- smplayer/trunk/src/languages/main.cpp
+++ smplayer/trunk/src/languages/main.cpp
@@ -8,7 +8,7 @@ int main( int argc, char ** argv )
 	if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
 		return -1;
 
-	QRegExp rx("^([a-zA-Z]+) ([a-zA-Z]+)");
+	QRegExp rx("^([a-zA-Z]+) ([a-zA-Z\\-]+)");
 
 	QString line;
 	while (!file.atEnd()) {
