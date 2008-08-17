2ea6daedf736895e575cc94a1af408a7656bf743
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2008-08-17 00:02:33 +0000

    Added m2ts to the list of extensions for video.

--- smplayer/trunk/src/extensions.cpp
+++ smplayer/trunk/src/extensions.cpp
@@ -51,7 +51,7 @@ Extensions::Extensions()
            << "bin" << "iso" << "vob"
            << "mkv" << "nsv" << "ram" << "flv"
            << "rm" << "swf"
-           << "ts" << "rmvb" << "dvr-ms" << "m2t" << "rec";
+           << "ts" << "rmvb" << "dvr-ms" << "m2t" << "m2ts" << "rec";
 
 	_audio << "mp3" << "ogg" << "wav" << "wma" <<  "ac3" << "ra" << "ape" << "flac";
 
