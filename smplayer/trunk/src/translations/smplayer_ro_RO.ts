b2549700e969de0d4cb37576b7ae0e1eb887337c
DoruHush <DoruHush@fd4b1137-b938-0410-b384-8fdad0efb714>
2008-09-26 00:07:33 +0000

    "Updated the Romanian Translation"

--- smplayer/trunk/src/translations/smplayer_ro_RO.ts
+++ smplayer/trunk/src/translations/smplayer_ro_RO.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="en_US">
+<defaultcodec></defaultcodec>
 <context>
     <name>About</name>
     <message>
@@ -290,7 +291,7 @@
     <message>
         <location filename="../about.cpp" line="183"/>
         <source>Galician</source>
-        <translation type="unfinished">Galițiană</translation>
+        <translation>Galițiană</translation>
     </message>
 </context>
 <context>
