df336c470fb4f6f59d98da7a817f404a7245c2b4
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2008-10-12 16:44:36 +0000

    Run 'aplay' instead of '/usr/bin/aplay'

--- smplayer/trunk/src/deviceinfo.cpp
+++ smplayer/trunk/src/deviceinfo.cpp
@@ -69,27 +69,27 @@ DeviceList DeviceInfo::alsaDevices() {
 	DeviceList l;
 	QRegExp rx_device("^card\\s([0-9]+).*\\[(.*)\\],\\sdevice\\s([0-9]+):");
 
-	if (QFile::exists("/usr/bin/aplay")) {
-		QProcess p;
-		p.setProcessChannelMode( QProcess::MergedChannels );
-		p.setEnvironment( QStringList() << "LC_ALL=C" );
-		p.start("/usr/bin/aplay", QStringList() << "-l");
+	QProcess p;
+	p.setProcessChannelMode( QProcess::MergedChannels );
+	p.setEnvironment( QStringList() << "LC_ALL=C" );
+	p.start("aplay", QStringList() << "-l");
 
-		if (p.waitForFinished()) {
-			QByteArray line;
-			while (p.canReadLine()) {
-				line = p.readLine();
-				qDebug("DeviceInfo::alsaDevices: '%s'", line.constData());
-				if ( rx_device.indexIn(line) > -1 ) {
-					QString id = rx_device.cap(1);
-					id.append(".");
-					id.append(rx_device.cap(3));
-					QString desc = rx_device.cap(2);
-					qDebug("DeviceInfo::alsaDevices: found device: '%s' '%s'", id.toUtf8().constData(), desc.toUtf8().constData());
-					l.append( DeviceData(id, desc) );
-				}
+	if (p.waitForFinished()) {
+		QByteArray line;
+		while (p.canReadLine()) {
+			line = p.readLine();
+			qDebug("DeviceInfo::alsaDevices: '%s'", line.constData());
+			if ( rx_device.indexIn(line) > -1 ) {
+				QString id = rx_device.cap(1);
+				id.append(".");
+				id.append(rx_device.cap(3));
+				QString desc = rx_device.cap(2);
+				qDebug("DeviceInfo::alsaDevices: found device: '%s' '%s'", id.toUtf8().constData(), desc.toUtf8().constData());
+				l.append( DeviceData(id, desc) );
 			}
 		}
+	} else {
+		qDebug("DeviceInfo::alsaDevices: could not start aplay, error %d", p.error());
 	}
 
 	return l;
