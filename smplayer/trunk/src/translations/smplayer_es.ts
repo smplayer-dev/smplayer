7b12346090df6a6ffb5b5ce4e2043c972f92a641
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2008-10-31 01:21:04 +0000

    Updated the Spanish translation

--- smplayer/trunk/src/translations/smplayer_es.ts
+++ smplayer/trunk/src/translations/smplayer_es.ts
@@ -6029,7 +6029,7 @@ For translators: don&apos;t translate this text, it will be replaced with anothe
     <message>
         <location filename="../prefsubtitles.cpp" line="507"/>
         <source>If border style is se to outline, this specifies the width of the outline around the text in pixels.</source>
-        <translation>Si el estilo del borde está establecido en contorno, esta opción especifica el ancho del contorno alrededor del text en píxeles.</translation>
+        <translation type="obsolete">Si el estilo del borde está establecido en contorno, esta opción especifica el ancho del contorno alrededor del text en píxeles.</translation>
     </message>
     <message>
         <location filename="../prefsubtitles.cpp" line="509"/>
@@ -6039,7 +6039,7 @@ For translators: don&apos;t translate this text, it will be replaced with anothe
     <message>
         <location filename="../prefsubtitles.cpp" line="511"/>
         <source>If border style is se to outline, this specifies the depth of the drop shadow behind the text in pixels.</source>
-        <translation>Si el estilo del borde está establecido en contorno, esta opción especifica la profundidad de la sombra tras el texto en píxeles.</translation>
+        <translation type="obsolete">Si el estilo del borde está establecido en contorno, esta opción especifica la profundidad de la sombra tras el texto en píxeles.</translation>
     </message>
     <message>
         <location filename="../prefsubtitles.ui" line="930"/>
@@ -6173,6 +6173,16 @@ For translators: don&apos;t translate this text, it will be replaced with anothe
         <source>Opaque box</source>
         <comment>border style</comment>
         <translation>Opaco</translation>
+    </message>
+    <message>
+        <location filename="../prefsubtitles.cpp" line="507"/>
+        <source>If border style is set to &lt;i&gt;outline&lt;/i&gt;, this option specifies the width of the outline around the text in pixels.</source>
+        <translation>Si el estilo del borde está establecido en &lt;i&gt;contorno&lt;/i&gt;, esta opción especifica el ancho del contorno alrededor del texto en píxeles.</translation>
+    </message>
+    <message>
+        <location filename="../prefsubtitles.cpp" line="511"/>
+        <source>If border style is set to &lt;i&gt;outline&lt;/i&gt;, this option specifies the depth of the drop shadow behind the text in pixels.</source>
+        <translation>Si el estilo del borde está establecido en &lt;i&gt;contorno&lt;/i&gt;, esta opción especifica la profundidad de la sombra tras el texto en píxeles.</translation>
     </message>
 </context>
 <context>
@@ -6465,11 +6475,6 @@ For translators: don&apos;t translate this text, it will be replaced with anothe
         <location filename="../timedialog.ui" line="57"/>
         <source>&amp;Jump to:</source>
         <translation>&amp;Saltar a:</translation>
-    </message>
-    <message>
-        <location filename="../timedialog.ui" line="70"/>
-        <source>H:mm:ss</source>
-        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
