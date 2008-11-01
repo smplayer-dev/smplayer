d4481efa2c04537056ac14b2ed8760b230548a01
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2008-11-01 02:01:40 +0000

    1 new text to translate

--- smplayer/trunk/src/translations/smplayer_en_US.ts
+++ smplayer/trunk/src/translations/smplayer_en_US.ts
@@ -5976,11 +5976,6 @@ For translators: don&apos;t translate this text, it will be replaced with anothe
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../prefsubtitles.cpp" line="433"/>
-        <source>Click this button to enable the the new SSA/ASS library. This allows to display subtitles with multiple colors, fonts...</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../prefsubtitles.cpp" line="435"/>
         <source>Normal subtitles</source>
         <translation type="unfinished"></translation>
@@ -6048,6 +6043,11 @@ For translators: don&apos;t translate this text, it will be replaced with anothe
     <message>
         <location filename="../prefsubtitles.ui" line="908"/>
         <source>&amp;Line spacing:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefsubtitles.cpp" line="433"/>
+        <source>Click this button to enable the new SSA/ASS library. This allows to display subtitles with multiple colors, fonts...</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
