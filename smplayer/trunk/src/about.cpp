db5a931da0cc71e7aef66aeb2be830c47853ed71
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2009-01-01 02:25:02 +0000

    Changed the copyright year

--- smplayer/trunk/src/about.cpp
+++ smplayer/trunk/src/about.cpp
@@ -1,5 +1,5 @@
 /*  smplayer, GUI front-end for mplayer.
-    Copyright (C) 2006-2008 Ricardo Villalba <rvm@escomposlinux.org>
+    Copyright (C) 2006-2009 Ricardo Villalba <rvm@escomposlinux.org>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -49,7 +49,7 @@ About::About(QWidget * parent, Qt::WindowFlags f)
 	}
 
 	info->setText( 
-		"<b>SMPlayer</b> &copy; 2006-2008 Ricardo Villalba &lt;rvm@escomposlinux.org&gt;<br><br>"
+		"<b>SMPlayer</b> &copy; 2006-2009 Ricardo Villalba &lt;rvm@escomposlinux.org&gt;<br><br>"
 		"<b>" + tr("Version: %1").arg(smplayerVersion()) + "</b>" +
 #if PORTABLE_APP
                 " (" + tr("Portable Edition") + ")" +
