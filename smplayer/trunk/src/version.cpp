2bff2e97ad9575781e03903b39ab277bedd263ab
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2009-03-09 23:39:55 +0000

    Changed the version to 0.6.7

--- smplayer/trunk/src/version.cpp
+++ smplayer/trunk/src/version.cpp
@@ -18,9 +18,9 @@
 
 #include "version.h"
 
-#define USE_SVN_VERSIONS 1
+#define USE_SVN_VERSIONS 0
 
-#define VERSION "0.6.6"
+#define VERSION "0.6.7 (SVN r2829)"
 
 #if USE_SVN_VERSIONS
 #include "svn_revision.h"
