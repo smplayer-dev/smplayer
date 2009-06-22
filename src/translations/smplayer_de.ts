c595d749723c0363b503d15d5bddadefe048a877
henrikx <henrikx@fd4b1137-b938-0410-b384-8fdad0efb714>
2009-06-22 07:43:19 +0000

    Übersetzung

--- src/translations/smplayer_de.ts
+++ src/translations/smplayer_de.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="de_DE">
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
 <context>
     <name>About</name>
     <message>
@@ -1585,13 +1586,13 @@ Das kann im folgenden Konfigurationsdialog gemacht werden.</translation>
     <message>
         <location filename="../basegui.cpp" line="1413"/>
         <source>Next wheel function</source>
-        <translation type="unfinished"></translation>
+        <translation>Nächste Mausrad Funktion</translation>
     </message>
     <message>
         <location filename="../basegui.cpp" line="1563"/>
         <source>P&amp;rogram</source>
         <comment>program</comment>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Programm</translation>
     </message>
 </context>
 <context>
@@ -3404,7 +3405,7 @@ Auswahl des Ordners, der das VIDEO_TS und das AUDIO_TS Verzeichnis enthält.</tr
         <source>Marshallese</source>
         <translation>Marshallese</translation>
     </message>
-    <message encoding="UTF-8">
+    <message utf8="true">
         <location filename="../languages.cpp" line="136"/>
         <source>Bokmål</source>
         <translation>Bokmål</translation>
@@ -3514,7 +3515,7 @@ Auswahl des Ordners, der das VIDEO_TS und das AUDIO_TS Verzeichnis enthält.</tr
         <source>Venda</source>
         <translation>Venda</translation>
     </message>
-    <message encoding="UTF-8">
+    <message utf8="true">
         <location filename="../languages.cpp" line="200"/>
         <source>Volapük</source>
         <translation>Volapük</translation>
