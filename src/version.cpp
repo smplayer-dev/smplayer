b1cb0e1067f6f25ed44b675b9c21500448a83a0d
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2009-07-20 10:38:03 +0000

    Changed the version to 0.6.8

--- src/version.cpp
+++ src/version.cpp
@@ -18,9 +18,9 @@
 
 #include "version.h"
 
-#define USE_SVN_VERSIONS 1
+#define USE_SVN_VERSIONS 0
 
-#define VERSION "0.6.7"
+#define VERSION "0.6.8 (SVN r3213)"
 
 #if USE_SVN_VERSIONS
 #include "svn_revision.h"
