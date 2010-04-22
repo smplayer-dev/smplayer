f343e1ef378815029be3fa278a5a1e4aeba19360
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2010-04-22 21:12:49 +0000

    Added f4v to the list of extensions (for video).

--- src/extensions.cpp
+++ src/extensions.cpp
@@ -51,7 +51,8 @@ Extensions::Extensions()
            << "bin" << "iso" << "vob"
            << "mkv" << "nsv" << "ram" << "flv"
            << "rm" << "swf"
-           << "ts" << "rmvb" << "dvr-ms" << "m2t" << "m2ts" << "rec";
+           << "ts" << "rmvb" << "dvr-ms" << "m2t" << "m2ts" << "rec"
+           << "f4v" ;
 
 	_audio << "mp3" << "ogg" << "wav" << "wma" <<  "ac3" << "ra" << "ape" << "flac";
 
