0d2623bf4a50dc7574423a2a0adf642b5761b5a9
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2012-01-27 23:54:47 +0000

    Simplify the about dialog

--- smplayer/trunk/src/paths.h
+++ smplayer/trunk/src/paths.h
@@ -34,7 +34,7 @@ public:
 	static QString themesPath();
 	static QString shortcutsPath();
 	static QString qtTranslationPath();
-	static QString doc(QString file, QString locale = QString::null);
+	static QString doc(QString file, QString locale = QString::null, bool english_fallback = true);
 
 	//! Forces to use a different path for the config files
 	static void setConfigPath(QString path);
