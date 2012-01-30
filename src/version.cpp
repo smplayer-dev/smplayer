520ffe5f158173a6a2a387a86ed2916d82fa90c4
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2012-01-30 11:17:41 +0000

    Change version to 0.7.0

--- src/version.cpp
+++ src/version.cpp
@@ -18,9 +18,9 @@
 
 #include "version.h"
 
-#define USE_SVN_VERSIONS 1
+#define USE_SVN_VERSIONS 0
 
-#define VERSION "0.6.10"
+#define VERSION "0.7.0 (SVN r3809)"
 
 #if USE_SVN_VERSIONS
 #include "svn_revision.h"
