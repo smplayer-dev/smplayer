80576ff79c112d2057a08c29fc3cbf18691d11f7
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2012-03-29 21:50:58 +0000

    Some changes for OS2

--- smplayer/trunk/get_svn_revision.sh
+++ smplayer/trunk/get_svn_revision.sh
@@ -7,10 +7,14 @@
 test "$1" && extra="-$1"
 
 svn_revision=`LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2`
+test $svn_revision || svn_revision=`cd "$1" && LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2`
 test $svn_revision || svn_revision=`cd "$1" && grep revision .svn/entries 2>/dev/null | cut -d '"' -f2`
 test $svn_revision || svn_revision=`cd "$1" && sed -n -e '/^dir$/{n;p;q}' .svn/entries 2>/dev/null`
 test $svn_revision || svn_revision=0UNKNOWN
 
+if test "$2"; then
+extra="-$2"
+fi
 NEW_REVISION="#define SVN_REVISION \"SVN-r${svn_revision}${extra}\""
 OLD_REVISION=`cat src/svn_revision.h 2> /dev/null`
 
