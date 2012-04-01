9bed47a60c9117e6d4e814a965ddbc8bdf4dbf98
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2012-04-01 20:52:18 +0000

    Fix a plural

--- src/findsubtitles/findsubtitleswindow.cpp
+++ src/findsubtitles/findsubtitleswindow.cpp
@@ -561,7 +561,7 @@ bool FindSubtitlesWindow::uncompressZip(const QString & filename, const QString 
 			qDebug("FindSubtitlesWindow::uncompressZip: extracted %s ok: %d", file.toUtf8().constData(), ok);
 			if (ok) extracted_count++;
 		}
-		status->setText(tr("%1 subtitle(s) extracted","", extracted_count).arg(extracted_count));
+		status->setText(tr("%n subtitle(s) extracted","", extracted_count));
 		if (extracted_count > 0) {
 			emit subtitleDownloaded( output_path +"/"+ files_to_extract[0] );
 		}
