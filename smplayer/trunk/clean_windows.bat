be561bc8ed080d22fd03fe9174058938f1c68063
redxii <redxii@fd4b1137-b938-0410-b384-8fdad0efb714>
2012-06-01 16:02:48 +0000

    Clean more stuff left behing from build process

--- smplayer/trunk/clean_windows.bat
+++ smplayer/trunk/clean_windows.bat
@@ -6,4 +6,8 @@ mingw32-make distclean
 
 cd ..
 del src\translations\smplayer_*.qm
-
+del src\object_script.smplayer.Release
+del src\object_script.smplayer.Debug
+del src\svn_revision.h
+rd src\release
+rd src\debug
