95f9d2b7d282360f0b35c226db70fb1d7d9bcb14
redxii <redxii@fd4b1137-b938-0410-b384-8fdad0efb714>
2012-09-12 07:41:50 +0000

    Validate input, make sure files/folders exist, and add NSIS option

--- smplayer/trunk/setup/scripts/make_pkgs.cmd
+++ smplayer/trunk/setup/scripts/make_pkgs.cmd
@@ -12,18 +12,21 @@ echo 7zip command-line (http://7zip.org) is required by this script.
 echo.
 echo * Release Packages
 echo.
-echo 1 - Portable SMPlayer Package
-echo 2 - SMPlayer Package w/o MPlayer
-echo 3 - MPlayer Package
+echo 1 - NSIS SMPlayer Packages
+echo 2 - Portable SMPlayer Package
+echo 3 - SMPlayer Package w/o MPlayer
+echo 4 - MPlayer Package
 echo.
 echo * Update Packages
 echo.
-echo 4 - SMPlayer SVN Update Package
-echo 5 - Qt DLL Package
+echo 5 - SMPlayer SVN Update Package
+echo 6 - Qt DLL Package
 
 echo.
 
 set TOP_LEVEL_DIR=..
+
+set NSIS_PATH="C:\Program Files (x86)\NSIS\Unicode"
 
 set SMPLAYER_DIR=%TOP_LEVEL_DIR%\smplayer-build
 set MPLAYER_DIR=%TOP_LEVEL_DIR%\mplayer
@@ -35,12 +38,54 @@ set QT_DIR=E:\Buildsys\Qt\%QTVER%
 set /P USER_CHOICE="Choose an action: "
 echo.
 
-if "%USER_CHOICE%" == "1"     goto portable
-if "%USER_CHOICE%" == "2"     goto nomplayer
-if "%USER_CHOICE%" == "3"     goto mplayer
-if "%USER_CHOICE%" == "4"     goto svn_updpkg
-if "%USER_CHOICE%" == "5"     goto qtdlls
-if "%USER_CHOICE%" == ""      goto end
+if "%USER_CHOICE%" == "1" (
+
+  goto nsispkg
+
+) else if "%USER_CHOICE%" == "2" (
+
+  goto portable
+
+) else if "%USER_CHOICE%" == "3" (
+
+  goto nomplayer
+
+) else if "%USER_CHOICE%" == "4" (
+
+  goto mplayer
+
+) else if "%USER_CHOICE%" == "5" (
+
+  goto svn_updpkg
+
+) else if "%USER_CHOICE%" == "6" (
+
+  goto qtdlls
+
+) else (
+
+  goto reask
+
+)
+
+:nsispkg
+
+set /P VER_MAJOR="VER_MAJOR: "
+set /P VER_MINOR="VER_MINOR: "
+set /P VER_BUILD="VER_BUILD: "
+set /P VER_REVISION="VER_REVISION: "
+
+echo.
+
+%NSIS_PATH%\makensis.exe /DVER_MAJOR=%VER_MAJOR% /DVER_MINOR=%VER_MINOR% /DVER_BUILD=%VER_BUILD% /DVER_REVISION=%VER_REVISION% %TOP_LEVEL_DIR%\smplayer.nsi
+
+rem %NSIS_PATH%\makensis.exe /DVER_MAJOR=%VER_MAJOR% /DVER_MINOR=%VER_MINOR% /DVER_BUILD=%VER_BUILD% /DVER_REVISION=%VER_REVISION% /DWIN64 %TOP_LEVEL_DIR%\smplayer.nsi
+
+rem %NSIS_PATH%\makensis.exe /DVER_MAJOR=%VER_MAJOR% /DVER_MINOR=%VER_MINOR% /DVER_BUILD=%VER_BUILD% /DVER_REVISION=%VER_REVISION% /DWIN64 %TOP_LEVEL_DIR%\smplayer.full.nsi
+
+echo.
+
+goto end
 
 :portable
 echo --- Creating SMPlayer Portable Package ---
@@ -50,78 +95,93 @@ set /P SMPLAYER_VER="SMPlayer Version: "
 if "%SMPLAYER_VER%"=="" exit
 echo.
 
+if not exist %PORTABLE_EXE_DIR%\smplayer-portable.exe (
+  echo SMPlayer portable EXE not found!
+	goto end
+)
+
+if not exist %PORTABLE_EXE_DIR%\smtube-portable.exe (
+  echo SMTube portable EXE not found!
+	goto end
+)
+
 ren %SMPLAYER_DIR% smplayer-portable-%SMPLAYER_VER%
-set SMPLAYER_DIR=%TOP_LEVEL_DIR%\smplayer-portable-%SMPLAYER_VER%
+set SMPLAYER_PORTABLE_DIR=%TOP_LEVEL_DIR%\smplayer-portable-%SMPLAYER_VER%
+
+if not exist %TOP_LEVEL_DIR%\smplayer-portable-%SMPLAYER_VER% (
+  echo Oops! Unable to find renamed directory, make sure no files are opened.
+	goto end
+)
 
 echo.
 echo ######      Backing up files       #######
 echo.
 
-ren %SMPLAYER_DIR%\smplayer.exe smplayer.bak
-ren %SMPLAYER_DIR%\smtube.exe smtube.bak
-ren %SMPLAYER_DIR%\mplayer\mplayer\config config.bak
+ren %SMPLAYER_PORTABLE_DIR%\smplayer.exe smplayer.bak
+ren %SMPLAYER_PORTABLE_DIR%\smtube.exe smtube.bak
+ren %SMPLAYER_PORTABLE_DIR%\mplayer\mplayer\config config.bak
 
 echo.
 echo ######   Create screenshots dir.   #######
 echo.
 
-mkdir %SMPLAYER_DIR%\screenshots
+mkdir %SMPLAYER_PORTABLE_DIR%\screenshots
 
 echo.
 echo ######     Create smplayer.ini     #######
 echo.
 
-echo [%%General]>> %SMPLAYER_DIR%\smplayer.ini
-echo screenshot_directory=.\\screenshots>> %SMPLAYER_DIR%\smplayer.ini
-echo.>> %SMPLAYER_DIR%\smplayer.ini
-echo [advanced]>> %SMPLAYER_DIR%\smplayer.ini
-echo mplayer_additional_options=-nofontconfig>> %SMPLAYER_DIR%\smplayer.ini
+echo [%%General]>> %SMPLAYER_PORTABLE_DIR%\smplayer.ini
+echo screenshot_directory=.\\screenshots>> %SMPLAYER_PORTABLE_DIR%\smplayer.ini
+echo.>> %SMPLAYER_PORTABLE_DIR%\smplayer.ini
+echo [advanced]>> %SMPLAYER_PORTABLE_DIR%\smplayer.ini
+echo mplayer_additional_options=-nofontconfig>> %SMPLAYER_PORTABLE_DIR%\smplayer.ini
 
 echo.
 echo ######  Create smplayer_orig.ini   #######
 echo.
 
-echo [%%General]>> %SMPLAYER_DIR%\smplayer_orig.ini
-echo screenshot_directory=.\\screenshots>> %SMPLAYER_DIR%\smplayer_orig.ini
-echo.>> %SMPLAYER_DIR%\smplayer_orig.ini
-echo [advanced]>> %SMPLAYER_DIR%\smplayer_orig.ini
-echo mplayer_additional_options=-nofontconfig>> %SMPLAYER_DIR%\smplayer_orig.ini
+echo [%%General]>> %SMPLAYER_PORTABLE_DIR%\smplayer_orig.ini
+echo screenshot_directory=.\\screenshots>> %SMPLAYER_PORTABLE_DIR%\smplayer_orig.ini
+echo.>> %SMPLAYER_PORTABLE_DIR%\smplayer_orig.ini
+echo [advanced]>> %SMPLAYER_PORTABLE_DIR%\smplayer_orig.ini
+echo mplayer_additional_options=-nofontconfig>> %SMPLAYER_PORTABLE_DIR%\smplayer_orig.ini
 
 echo.
 echo ######    Create mplayer config    #######
 echo.
 
-echo ## MPlayer Windows configuration>> %SMPLAYER_DIR%\mplayer\mplayer\config
-echo.>> %SMPLAYER_DIR%\mplayer\mplayer\config
-echo subfont=c:\windows\fonts\arial.ttf>> %SMPLAYER_DIR%\mplayer\mplayer\config
-echo ^<cachedir^>../fontconfig^</cachedir^>> %SMPLAYER_DIR%\mplayer\fonts\local.conf
+echo ## MPlayer Windows configuration>> %SMPLAYER_PORTABLE_DIR%\mplayer\mplayer\config
+echo.>> %SMPLAYER_PORTABLE_DIR%\mplayer\mplayer\config
+echo subfont=c:\windows\fonts\arial.ttf>> %SMPLAYER_PORTABLE_DIR%\mplayer\mplayer\config
+echo ^<cachedir^>../fontconfig^</cachedir^>> %SMPLAYER_PORTABLE_DIR%\mplayer\fonts\local.conf
 
 echo.
 echo ######    Copying portable .exe    #######
 echo.
 
-copy /y %PORTABLE_EXE_DIR%\smplayer-portable.exe %SMPLAYER_DIR%\smplayer.exe
-copy /y %PORTABLE_EXE_DIR%\smtube-portable.exe %SMPLAYER_DIR%\smtube.exe
+copy /y %PORTABLE_EXE_DIR%\smplayer-portable.exe %SMPLAYER_PORTABLE_DIR%\smplayer.exe
+copy /y %PORTABLE_EXE_DIR%\smtube-portable.exe %SMPLAYER_PORTABLE_DIR%\smtube.exe
 
 echo.
 echo ######  Creating portable package  #######
 echo.
-7za a -t7z %OUTPUT_DIR%\smplayer-portable-%SMPLAYER_VER%.7z %SMPLAYER_DIR% -xr!*.bak -xr!qxtcore.dll -mx9
+7za a -t7z %OUTPUT_DIR%\smplayer-portable-%SMPLAYER_VER%.7z %SMPLAYER_PORTABLE_DIR% -xr!*.bak -xr!qxtcore.dll -mx9
 
 echo.
 echo Restoring source folder(s) back to its original state...
 echo.
-rmdir %SMPLAYER_DIR%\screenshots
-del %SMPLAYER_DIR%\smplayer.ini
-del %SMPLAYER_DIR%\smplayer_orig.ini
-del %SMPLAYER_DIR%\smplayer.exe
-del %SMPLAYER_DIR%\smtube.exe
-del %SMPLAYER_DIR%\mplayer\mplayer\config
-del %SMPLAYER_DIR%\mplayer\fonts\local.conf
-ren %SMPLAYER_DIR%\smplayer.bak smplayer.exe
-ren %SMPLAYER_DIR%\smtube.bak smtube.exe
-ren %SMPLAYER_DIR%\mplayer\mplayer\config.bak config
-ren %SMPLAYER_DIR% smplayer-build
+rmdir %SMPLAYER_PORTABLE_DIR%\screenshots
+del %SMPLAYER_PORTABLE_DIR%\smplayer.ini
+del %SMPLAYER_PORTABLE_DIR%\smplayer_orig.ini
+del %SMPLAYER_PORTABLE_DIR%\smplayer.exe
+del %SMPLAYER_PORTABLE_DIR%\smtube.exe
+del %SMPLAYER_PORTABLE_DIR%\mplayer\mplayer\config
+del %SMPLAYER_PORTABLE_DIR%\mplayer\fonts\local.conf
+ren %SMPLAYER_PORTABLE_DIR%\smplayer.bak smplayer.exe
+ren %SMPLAYER_PORTABLE_DIR%\smtube.bak smtube.exe
+ren %SMPLAYER_PORTABLE_DIR%\mplayer\mplayer\config.bak config
+ren %SMPLAYER_PORTABLE_DIR% smplayer-build
 
 goto end
 
@@ -155,7 +215,7 @@ set /P MP_REV="MPlayer Revision: "
 ren %MPLAYER_DIR% mplayer-svn-%MP_REV%
 set MPLAYER_DIR=%TOP_LEVEL_DIR%\mplayer-svn-%MP_REV%
 
-7za a -t7z %OUTPUT_DIR%\mplayer-svn-%MP_REV%.7z %MPLAYER_DIR% -mx9
+7za a -t7z %OUTPUT_DIR%\mplayer-svn-%MP_REV%.7z %MPLAYER_DIR% -xr!mencoder.exe -mx9
 
 ren %MPLAYER_DIR% mplayer
 set MPLAYER_DIR=%TOP_LEVEL_DIR%\mplayer
