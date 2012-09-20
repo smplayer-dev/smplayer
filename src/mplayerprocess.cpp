79d311678bf5de7af82078d316847bc54ed65e35
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2012-09-20 12:45:00 +0000

    Hack to recognize the mplayer version in ubuntu precise

--- src/mplayerprocess.cpp
+++ src/mplayerprocess.cpp
@@ -301,6 +301,7 @@ void MplayerProcess::parseLine(QByteArray ba) {
 		// Emulates mplayer version in Ubuntu:
 		//if (line.startsWith("MPlayer 1.0rc1")) line = "MPlayer 2:1.0~rc1-0ubuntu13.1 (C) 2000-2006 MPlayer Team";
 		//if (line.startsWith("MPlayer2")) line = "mplayer2 d0305da (C) 2000-2012 MPlayer & mplayer2 teams";
+		//if (line.startsWith("MPlayer SVN")) line = "MPlayer svn r34540 (Ubuntu), built with gcc-4.6 (C) 2000-2012 MPlayer Team";
 
 		// Emulates unknown version
 		//if (line.startsWith("MPlayer SVN")) line = "MPlayer lalksklsjjakksja";
