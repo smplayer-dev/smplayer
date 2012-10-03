4902b0662d5e4e99b84b6b786e17bf4a4ffbd51b
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2012-10-03 00:49:49 +0000

    Set the mplayer 1.1 revision

--- smplayer/trunk/src/mplayerversion.h
+++ smplayer/trunk/src/mplayerversion.h
@@ -25,9 +25,7 @@
 #define MPLAYER_1_0_RC2_SVN 24722
 #define MPLAYER_1_0_RC3_SVN 31272
 #define MPLAYER_1_0_RC4_SVN 33472
-
-// Fix me: find out the revision of version 1.1
-#define MPLAYER_1_1 33473
+#define MPLAYER_1_1 34992
 
 class MplayerVersion {
 public:
