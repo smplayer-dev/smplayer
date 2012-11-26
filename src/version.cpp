93459465e459c0f31bf4bdb48dc2268a76614549
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2012-11-26 10:02:37 +0000

    Change version to 0.8.2.1 (release for linux only)

--- src/version.cpp
+++ src/version.cpp
@@ -18,9 +18,9 @@
 
 #include "version.h"
 
-#define USE_SVN_VERSIONS 1
+#define USE_SVN_VERSIONS 0
 
-#define VERSION "0.8.2"
+#define VERSION "0.8.2.1"
 
 #if USE_SVN_VERSIONS
 #include "svn_revision.h"
