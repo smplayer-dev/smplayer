a1b42177e6592c54adc057af7227c039edea8679
redxii <redxii@fd4b1137-b938-0410-b384-8fdad0efb714>
2012-12-23 01:10:29 +0000

    Properly check for errorlevel, reset to 0 and convert tabs to spaces

--- compile_windows.cmd
+++ compile_windows.cmd
@@ -9,34 +9,34 @@ set qmake_defs=
 :arg_loop
 if [%1]==[] (
 
-	goto compile
+  goto compile
 
 ) else if [%1]==[pe] (
 
-	set qmake_defs="DEFINES+=PORTABLE_APP"
-	set smtube_params=pe
+  set qmake_defs="DEFINES+=PORTABLE_APP"
+  set smtube_params=pe
 
 ) else if [%1]==[nosmtube] (
 
-	set build_smtube=false
+  set build_smtube=false
 
 ) else if [%1]==[-h] (
 
-	echo How to use:
-	echo.
-	echo Add ^`pe^' to compile portable.
-	echo Add ^`nosmtube^' to disable compiling smtube.
-	echo.
-	echo To compile SMPlayer/SMTube non-portable, enter no arguments.
-	echo.
-	echo ex: %script_name% pe nosmtube
-	goto end
+  echo How to use:
+  echo.
+  echo Add ^`pe^' to compile portable.
+  echo Add ^`nosmtube^' to disable compiling smtube.
+  echo.
+  echo To compile SMPlayer/SMTube non-portable, enter no arguments.
+  echo.
+  echo ex: %script_name% pe nosmtube
+  goto end
 
 ) else (
 
-	echo configure: error: unrecognized option: `%1'
-	echo Try `%script_name% -h' for more information
-	goto end
+  echo configure: error: unrecognized option: `%1'
+  echo Try `%script_name% -h' for more information
+  goto end
 
 ) 
 
@@ -55,20 +55,22 @@ lrelease smplayer.pro
 qmake %qmake_defs%
 mingw32-make
 
-if [%errorlevel%]==[0] (
-	if [%build_smtube%]==[true] (
-		if exist %smtube_svn_dir%\compile_windows.cmd (
+if ERRORLEVEL 0 (
+  if [%build_smtube%]==[true] (
+    if exist %smtube_svn_dir%\compile_windows.cmd (
 
-			cd %smtube_svn_dir%
-			compile_windows.cmd %smtube_params%
+      cd %smtube_svn_dir%
+      compile_windows.cmd %smtube_params%
 
-		) else (
+    ) else (
 
-			echo SMTube not found in specified directory... skipping
-		)
-	)
+      echo SMTube not found in specified directory... skipping
+    )
+  )
 ) else (
 echo Compilation error, script aborted
+:: Resets errorlevel to 0
+ver >nul
 )
 
 :end
