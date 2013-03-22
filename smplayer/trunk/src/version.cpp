271cd4b1774e1917a5ee6682e16a6241155e449c
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2013-03-22 23:55:06 +0000

    Change version to 0.8.4

--- smplayer/trunk/src/version.cpp
+++ smplayer/trunk/src/version.cpp
@@ -18,9 +18,9 @@
 
 #include "version.h"
 
-#define USE_SVN_VERSIONS 1
+#define USE_SVN_VERSIONS 0
 
-#define VERSION "0.8.3"
+#define VERSION "0.8.4"
 
 #if USE_SVN_VERSIONS
 #include "svn_revision.h"
