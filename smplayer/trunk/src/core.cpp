e1af6f9a37430ae61f4af4af899662caffaa18fb
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2013-04-01 01:37:36 +0000

    Add the define USE_FONTCONFIG_OPTIONS to smplayer.pro

--- smplayer/trunk/src/core.cpp
+++ smplayer/trunk/src/core.cpp
@@ -1723,9 +1723,11 @@ void Core::startMplayer( QString file, double seek ) {
 		// Use the same font for OSD
 #if !defined(Q_OS_OS2)
 		if (!pref->ass_styles.fontname.isEmpty()) {
+			#ifdef USE_FONTCONFIG_OPTIONS
 			if (!pref->mplayer_is_mplayer2) { // -fontconfig removed from mplayer2
 				proc->addArgument("-fontconfig");
 			}
+			#endif
 			proc->addArgument("-font");
 			proc->addArgument( pref->ass_styles.fontname );
 		}
@@ -1746,17 +1748,21 @@ void Core::startMplayer( QString file, double seek ) {
 		if (pref->freetype_support) proc->addArgument("-noass");
 #if !defined(Q_OS_OS2)
 		if ( (pref->use_fontconfig) && (!pref->font_name.isEmpty()) ) {
+			#ifdef USE_FONTCONFIG_OPTIONS
 			if (!pref->mplayer_is_mplayer2) { // -fontconfig removed from mplayer2
 				proc->addArgument("-fontconfig");
 			}
+			#endif
 			proc->addArgument("-font");
 			proc->addArgument( pref->font_name );
 		}
 #endif
 		if ( (!pref->use_fontconfig) && (!pref->font_file.isEmpty()) ) {
+			#ifdef USE_FONTCONFIG_OPTIONS
 			if (!pref->mplayer_is_mplayer2) { // -nofontconfig removed from mplayer2
 				proc->addArgument("-nofontconfig");
 			}
+			#endif
 			proc->addArgument("-font");
 			proc->addArgument( pref->font_file );
 		}
