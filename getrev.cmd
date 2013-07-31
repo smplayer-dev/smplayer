3fe55a7cdc369c8cb773a2055ea30fb1f2ec6fd4
redxii1234 <redxii1234@fd4b1137-b938-0410-b384-8fdad0efb714>
2013-07-31 07:53:17 +0000

    Simplify getrev.cmd

--- getrev.cmd
+++ getrev.cmd
@@ -2,21 +2,19 @@
 
 :: Some SVN clients can use localized messages (e.g. SlikSVN), force English
 set LC_ALL=C
+set SVN_REV=
 
-:: Undefine for multiple runs in the same command prompt session
-set Revision=
-for /f "tokens=2" %%i in ('svn info ^| find "Revision: "') do set Revision=%%i
+for /f "tokens=2" %%i in ('svn info ^| find "Revision:"') do set SVN_REV=%%i
 
 :: Set to UNKNOWN if no svn or working copy
-if "%REVISION%"=="" (
-  set REVISION=UNKNOWN
-)
-
-if "%Revision%"=="UNKNOWN" (
-echo Unknown SVN revision. SVN missing in PATH or not a working copy.
+if "%SVN_REV%"=="" (
+  set SVN_REV=UNKNOWN
+  echo Unknown SVN revision. SVN missing in PATH or not a working copy.
 ) else (
-echo SVN Revision: %Revision%
+  echo SVN Revision: %SVN_REV%
 )
 echo.
 
-echo #define SVN_REVISION "%REVISION%">src\svn_revision.h
+echo #define SVN_REVISION "%SVN_REV%">src\svn_revision.h
+
+set SVN_REV=
