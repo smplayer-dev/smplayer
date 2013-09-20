54aef51816fbe1db82f8985dfe685a2957607eef
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2013-09-20 23:48:37 +0000

    Read additional info from script

--- smplayer/trunk/src/youtube/ytsig.h
+++ smplayer/trunk/src/youtube/ytsig.h
@@ -26,6 +26,8 @@ class YTSig
 public:
 	static QString aclara(const QString & text, const QString & player = "", const QString & function_name = QString::null);
 
+	static QString parsed_ts;
+
 #ifdef YT_USE_SCRIPT
 	static void setScriptFile(const QString & f) { script_file = f; reloadScriptFile(); };
 
