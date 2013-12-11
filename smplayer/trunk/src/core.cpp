0b40a07fd6aac8f9e0c91332be1621f40349682f
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2013-12-11 00:23:25 +0000

    Use -noflip-hebrew when using -ass. It seems to be necessary to display arabic subtitles correctly.

--- smplayer/trunk/src/core.cpp
+++ smplayer/trunk/src/core.cpp
@@ -1805,6 +1805,10 @@ void Core::startMplayer( QString file, double seek ) {
 		proc->addArgument( "-ass-font-scale");
 		proc->addArgument( QString::number(mset.sub_scale_ass) );
 
+		if (!pref->mplayer_is_mplayer2) {
+			proc->addArgument( "-noflip-hebrew" ); // It seems to be necessary to display arabic subtitles correctly when using -ass
+		}
+
 		if (!pref->force_ass_styles) {
 			// Load the styles.ass file
 			if (!QFile::exists(Paths::subtitleStyleFile())) {
