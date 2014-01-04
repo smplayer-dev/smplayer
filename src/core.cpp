c27c48fe4f10732928cddb845770e09ad6affeba
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2014-01-04 02:11:20 +0000

    Don't use the -playlist option if the url scheme is ffmpeg

--- src/core.cpp
+++ src/core.cpp
@@ -1478,8 +1478,10 @@ void Core::startMplayer( QString file, double seek ) {
 		qDebug("Core::startMplayer: checking if stream is a playlist");
 		qDebug("Core::startMplayer: url path: '%s'", url.path().toUtf8().constData());
 
-		QRegExp rx("\\.ram$|\\.asx$|\\.m3u$|\\.m3u8$|\\.pls$", Qt::CaseInsensitive);
-		url_is_playlist = (rx.indexIn(url.path()) != -1);
+		if (url.scheme().toLower() != "ffmpeg") {
+			QRegExp rx("\\.ram$|\\.asx$|\\.m3u$|\\.m3u8$|\\.pls$", Qt::CaseInsensitive);
+			url_is_playlist = (rx.indexIn(url.path()) != -1);
+		}
 	}
 	qDebug("Core::startMplayer: url_is_playlist: %d", url_is_playlist);
 
