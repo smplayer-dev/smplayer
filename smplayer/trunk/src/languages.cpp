b332ed7975a0b64f06512f426891cf1a6bec246f
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2014-03-13 00:53:01 +0000

    New translation: British English.

--- smplayer/trunk/src/languages.cpp
+++ smplayer/trunk/src/languages.cpp
@@ -240,6 +240,7 @@ QMap<QString,QString> Languages::translations() {
 	m["da"] = "Dansk"; //tr("Danish");
 	m["de"] = "Deutsch"; //tr("German");
 	m["el_GR"] = QString::fromUtf8("Ελληνικά"); //tr("Greek");
+	m["en_GB"] = "British English"; //tr("British English");
 	m["en_US"] = "English"; //tr("English");
 	m["es"] =  QString::fromUtf8("Español"); //tr("Spanish");
 	m["et"] = "Eesti"; //tr("Estonian");
