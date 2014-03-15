2ae50de2924bf79e1d0fc1a4b43c4c628e8af751
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2014-03-15 21:56:31 +0000

    Remove the variable add_to_playlist_consecutive_files

--- src/basegui.cpp
+++ src/basegui.cpp
@@ -3066,15 +3066,9 @@ void BaseGui::newMediaLoaded() {
 	if ((core->mdat.type == TYPE_FILE) && (pref->auto_add_to_playlist)) {
 		//qDebug("BaseGui::newMediaLoaded: playlist count: %d", playlist->count());
 		QStringList files_to_add;
-		#if 1
 		if (playlist->count() == 1) {
 			files_to_add = Helper::filesForPlaylist(core->mdat.filename, pref->media_to_add_to_playlist);
 		}
-		#else
-		if (pref->add_to_playlist_consecutive_files) {
-			files_to_add = Helper::filesForPlaylist(core->mdat.filename, Preferences::ConsecutiveFiles);
-		}
-		#endif
 		if (!files_to_add.empty()) playlist->addFiles(files_to_add);
 	}
 }
