f96a673069f568d9f9c90250a4a03ef3fdd92fd1
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2014-03-28 13:11:27 +0000

    (Windows) (Test) Catch WM_APPCOMMAND

--- smplayer/trunk/src/myapplication.cpp
+++ smplayer/trunk/src/myapplication.cpp
@@ -18,5 +18,41 @@
 
 #include "myapplication.h"
 
+#ifdef Q_OS_WIN
+#include <QKeyEvent>
+#include <QEvent>
+#include <QWidget>
+#include <windows.h>
+#include <QDebug>
+
+#ifndef WM_APPCOMMAND
+#define WM_APPCOMMAND 0x0319
+#endif
+
+#define FAPPCOMMAND_MASK  0xF000
+#define GET_APPCOMMAND_LPARAM(lParam) ((short)(HIWORD(lParam) & ~FAPPCOMMAND_MASK))
+
+#define APPCOMMAND_MEDIA_PLAY 46
+#define APPCOMMAND_MEDIA_STOP 13
+
+bool MyApplication::winEventFilter ( MSG * msg, long * result ) {
+	if (msg->message == WM_APPCOMMAND) {
+		qDebug() << "MyApplication::winEventFilter" << msg->message;
+		int cmd  = GET_APPCOMMAND_LPARAM(msg->lParam);
+		qDebug() << "MyApplication::winEventFilter: cmd" << cmd;
+		if (cmd == APPCOMMAND_MEDIA_PLAY) {
+			QKeyEvent event(QEvent::KeyPress, Qt::Key_MediaPlay, Qt::NoModifier, "Media Play");
+			QCoreApplication::sendEvent(QApplication::focusWidget(), &event);
+		}
+		else
+		if (cmd == APPCOMMAND_MEDIA_STOP) {
+			QKeyEvent event(QEvent::KeyPress, Qt::Key_MediaStop, Qt::NoModifier, "Media Stop");
+			QCoreApplication::sendEvent(QApplication::focusWidget(), &event);
+		}	
+	}
+	return false;
+}
+#endif
+
 #include "moc_myapplication.cpp"
 
