43fb6211a4a0641644d11d345967e3639f3d6032
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2014-08-19 00:22:10 +0000

    New version of the class Images, now it can load the icons from a resource file (but still disabled).

---
+++ smplayer/trunk/src/images.h
@@ -0,0 +1,62 @@
+/*  smplayer, GUI front-end for mplayer.
+    Copyright (C) 2006-2014 Ricardo Villalba <rvm@users.sourceforge.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifndef IMAGES_H
+#define IMAGES_H
+
+#include <QString>
+#include <QPixmap>
+#include <QIcon>
+
+//#define USE_RESOURCES
+#define SMCODE
+
+class Images
+{
+
+public:
+	static void setThemesPath(const QString & folder);
+	static void setTheme(const QString & name);
+
+	static QPixmap icon(QString name, int size=-1, bool png = true);
+	static QPixmap flippedIcon(QString name, int size=-1, bool png = true);
+
+	static QIcon multiIcon(QString name, QString fallback_icon);
+
+	//! Returns the filename of the icon
+	static QString file(const QString & icon_name);
+
+#ifdef SMCODE
+	static QString styleSheet();
+	static QString themesDirectory();
+#endif
+
+private:
+	static QPixmap resize(QPixmap *p, int size=20);
+	static QPixmap flip(QPixmap *p);
+
+	static QString current_theme;
+	static QString themes_path;
+
+#ifdef USE_RESOURCES
+	static QString resourceFilename();
+	static QString last_resource_loaded;
+#endif
+};
+
+#endif
