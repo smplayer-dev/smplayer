031980c41d505cb4edea6ae3976ee9cf5d027236
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2014-08-20 20:40:03 +0000

    Install the new theme files

--- smplayer/trunk/setup/scripts/install_smplayer.cmd
+++ smplayer/trunk/setup/scripts/install_smplayer.cmd
@@ -70,12 +70,94 @@ svn export --force %SMPLAYER_DIR%\docs %OUTPUT_DIR%\docs
 echo.
 echo ######         Icon Themes         #######
 echo.
-svn export --force %SMPLAYER_THEMES_DIR%\themes %OUTPUT_DIR%\themes
+
+mkdir %OUTPUT_DIR%\themes\
+
+mkdir %OUTPUT_DIR%\themes\Gartoon
+copy %SMPLAYER_THEMES_DIR%\themes\Gartoon\Gartoon.rcc %OUTPUT_DIR%\themes\Gartoon\
+copy %SMPLAYER_THEMES_DIR%\themes\Gartoon\README.txt %OUTPUT_DIR%\themes\Gartoon\
+
+mkdir %OUTPUT_DIR%\themes\Gnome
+copy %SMPLAYER_THEMES_DIR%\themes\Gnome\Gnome.rcc %OUTPUT_DIR%\themes\Gnome\
+copy %SMPLAYER_THEMES_DIR%\themes\Gnome\README.txt %OUTPUT_DIR%\themes\Gnome\
+
+mkdir %OUTPUT_DIR%\themes\Monochrome
+copy %SMPLAYER_THEMES_DIR%\themes\Monochrome\Monochrome.rcc %OUTPUT_DIR%\themes\Monochrome\
+copy %SMPLAYER_THEMES_DIR%\themes\Monochrome\README.txt %OUTPUT_DIR%\themes\Monochrome\
+
+mkdir %OUTPUT_DIR%\themes\Noia
+copy %SMPLAYER_THEMES_DIR%\themes\Noia\Noia.rcc %OUTPUT_DIR%\themes\Noia\
+copy %SMPLAYER_THEMES_DIR%\themes\Noia\README.txt %OUTPUT_DIR%\themes\Noia\
+
+mkdir %OUTPUT_DIR%\themes\Numix-remix
+copy %SMPLAYER_THEMES_DIR%\themes\Numix-remix\Numix-remix.rcc %OUTPUT_DIR%\themes\Numix-remix\
+copy %SMPLAYER_THEMES_DIR%\themes\Numix-remix\README.txt %OUTPUT_DIR%\themes\Numix-remix\
+
+mkdir %OUTPUT_DIR%\themes\Numix-uTouch
+copy %SMPLAYER_THEMES_DIR%\themes\Numix-uTouch\Numix-uTouch.rcc %OUTPUT_DIR%\themes\Numix-uTouch\
+copy %SMPLAYER_THEMES_DIR%\themes\Numix-uTouch\README.txt %OUTPUT_DIR%\themes\Numix-uTouch\
+
+mkdir %OUTPUT_DIR%\themes\Nuvola
+copy %SMPLAYER_THEMES_DIR%\themes\Nuvola\Nuvola.rcc %OUTPUT_DIR%\themes\Nuvola\
+copy %SMPLAYER_THEMES_DIR%\themes\Nuvola\README.txt %OUTPUT_DIR%\themes\Nuvola\
+
+mkdir %OUTPUT_DIR%\themes\Oxygen
+copy %SMPLAYER_THEMES_DIR%\themes\Oxygen\Oxygen.rcc %OUTPUT_DIR%\themes\Oxygen\
+copy %SMPLAYER_THEMES_DIR%\themes\Oxygen\README.txt %OUTPUT_DIR%\themes\Oxygen\
+
+mkdir %OUTPUT_DIR%\themes\Oxygen-Air
+copy %SMPLAYER_THEMES_DIR%\themes\Oxygen-Air\Oxygen-Air.rcc %OUTPUT_DIR%\themes\Oxygen-Air\
+copy %SMPLAYER_THEMES_DIR%\themes\Oxygen-Air\README.txt %OUTPUT_DIR%\themes\Oxygen-Air\
+
+mkdir %OUTPUT_DIR%\themes\Oxygen-Refit
+copy %SMPLAYER_THEMES_DIR%\themes\Oxygen-Refit\Oxygen-Refit.rcc %OUTPUT_DIR%\themes\Oxygen-Refit\
+copy %SMPLAYER_THEMES_DIR%\themes\Oxygen-Refit\README.txt %OUTPUT_DIR%\themes\Oxygen-Refit\
+
+mkdir %OUTPUT_DIR%\themes\Silk
+copy %SMPLAYER_THEMES_DIR%\themes\Silk\Silk.rcc %OUTPUT_DIR%\themes\Silk\
+copy %SMPLAYER_THEMES_DIR%\themes\Silk\README.txt %OUTPUT_DIR%\themes\Silk\
+
+mkdir %OUTPUT_DIR%\themes\Tango
+copy %SMPLAYER_THEMES_DIR%\themes\Tango\Tango.rcc %OUTPUT_DIR%\themes\Tango\
+copy %SMPLAYER_THEMES_DIR%\themes\Tango\README.txt %OUTPUT_DIR%\themes\Tango\
+
+mkdir %OUTPUT_DIR%\themes\blackPanther-Light
+copy %SMPLAYER_THEMES_DIR%\themes\blackPanther-Light\blackPanther-Light.rcc %OUTPUT_DIR%\themes\blackPanther-Light\
+copy %SMPLAYER_THEMES_DIR%\themes\blackPanther-Light\README.txt %OUTPUT_DIR%\themes\blackPanther-Light\
+
+mkdir %OUTPUT_DIR%\themes\blackPanther-Real
+copy %SMPLAYER_THEMES_DIR%\themes\blackPanther-Real\blackPanther-Real.rcc %OUTPUT_DIR%\themes\blackPanther-Real\
+copy %SMPLAYER_THEMES_DIR%\themes\blackPanther-Real\README.txt %OUTPUT_DIR%\themes\blackPanther-Real\
+
+mkdir %OUTPUT_DIR%\themes\blackPanther-VistaLike
+copy %SMPLAYER_THEMES_DIR%\themes\blackPanther-VistaLike\blackPanther-VistaLike.rcc %OUTPUT_DIR%\themes\blackPanther-VistaLike\
+copy %SMPLAYER_THEMES_DIR%\themes\blackPanther-VistaLike\README.txt %OUTPUT_DIR%\themes\blackPanther-VistaLike\
+
 
 echo.
 echo ######         Skins Themes         #######
 echo.
-svn export --force %SMPLAYER_SKINS_DIR%\themes %OUTPUT_DIR%\themes
+
+mkdir %OUTPUT_DIR%\themes\Black
+copy %SMPLAYER_SKINS_DIR%\themes\Black\Black.rcc %OUTPUT_DIR%\themes\Black\
+copy %SMPLAYER_SKINS_DIR%\themes\Black\main.css %OUTPUT_DIR%\themes\Black\
+
+mkdir %OUTPUT_DIR%\themes\Gonzo
+copy %SMPLAYER_SKINS_DIR%\themes\Gonzo\Gonzo.rcc %OUTPUT_DIR%\themes\Gonzo\
+copy %SMPLAYER_SKINS_DIR%\themes\Gonzo\main.css %OUTPUT_DIR%\themes\Gonzo\
+
+mkdir %OUTPUT_DIR%\themes\Mac
+copy %SMPLAYER_SKINS_DIR%\themes\Mac\Mac.rcc %OUTPUT_DIR%\themes\Mac\
+copy %SMPLAYER_SKINS_DIR%\themes\Mac\main.css %OUTPUT_DIR%\themes\Mac\
+
+mkdir %OUTPUT_DIR%\themes\Modern
+copy %SMPLAYER_SKINS_DIR%\themes\Modern\Modern.rcc %OUTPUT_DIR%\themes\Modern\
+copy %SMPLAYER_SKINS_DIR%\themes\Modern\main.css %OUTPUT_DIR%\themes\Modern\
+
+mkdir %OUTPUT_DIR%\themes\Vista
+copy %SMPLAYER_SKINS_DIR%\themes\Vista\Vista.rcc %OUTPUT_DIR%\themes\Vista\
+copy %SMPLAYER_SKINS_DIR%\themes\Vista\main.css %OUTPUT_DIR%\themes\Vista\
+
 
 echo.
 echo ######           MPlayer           #######
