ea85eae131d6db46b7b7e96cf2a9fcc9c247779d
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2014-08-31 00:20:12 +0000

    Disable the file debug message in images.cpp

--- smplayer/trunk/src/images.cpp
+++ smplayer/trunk/src/images.cpp
@@ -99,7 +99,7 @@ QString Images::file(const QString & name) {
 		icon_name = ":/icons-png/" + name + ".png";
 	}
 
-	qDebug() << "Images::file:" << icon_name;
+	//qDebug() << "Images::file:" << icon_name;
 	return icon_name;
 }
 
