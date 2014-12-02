eec5ed98dfa8db7249023fe6ba4df6282db10669
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2014-12-02 23:39:01 +0000

    Process -edl

--- src/mpvoptions.cpp
+++ src/mpvoptions.cpp
@@ -232,6 +232,10 @@ void MPVProcess::setOption(const QString & option_name, const QVariant & value) 
 		// Ignore
 	}
 	else
+	if (option_name == "edl") {
+		// Ignore
+	}
+	else
 	if (option_name == "tsprog") {
 		// Unsupported
 	}
