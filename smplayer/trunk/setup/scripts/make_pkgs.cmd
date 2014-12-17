17b5d9d8c7d3273d0793e294bc0bf188a7b9028c
redxii1234 <redxii1234@fd4b1137-b938-0410-b384-8fdad0efb714>
2014-12-17 06:08:01 +0000

    Remove MPV stuff

--- smplayer/trunk/setup/scripts/make_pkgs.cmd
+++ smplayer/trunk/setup/scripts/make_pkgs.cmd
@@ -6,9 +6,9 @@ if exist "turnon_echo" (
 :: Reset working dir especially when using 'Run as administrator'
 @cd /d "%~dp0"
 
-echo This batch file can help you to create a packages for SMPlayer and MPlayer/MPV.
+echo This batch file can help you to create a packages for SMPlayer and MPlayer.
 echo.
-echo This script will temporarily rename the smplayer-build and mplayer/mpv directories.
+echo This script will temporarily rename the smplayer-build and mplayer directories.
 echo Make sure these files ^& directories are not opened when running the script.
 echo.
 echo 1 - NSIS                          10 - NSIS [32-bit/64-bit]
@@ -17,11 +17,6 @@ echo 3 - Portable                      20 - Portable SFX [32-bit/64-bit]
 echo 4 - Portable [64-bit]
 echo 5 - Without MPlayer
 echo 6 - Without MPlayer [64-bit]
-echo.
-echo 31 - NSIS MPV                     40 - NSIS MPV [32-bit/64-bit]
-echo 32 - NSIS MPV [64-bit]            41 - Portable MPV [32-bit/64-bit]
-echo 33 - Portable MPV
-echo 34 - Portable MPV [64-bit]
 echo.
 
 :: Relative directory of all the source files to this script
@@ -62,7 +57,6 @@ where /q where.exe 2>NUL && (
 set SMPLAYER_DIR=%TOP_LEVEL_DIR%\smplayer-build
 set SMPLAYER_DIR64=%TOP_LEVEL_DIR%\smplayer-build64
 set MPLAYER_DIR=%TOP_LEVEL_DIR%\mplayer
-set MPV_DIR=%TOP_LEVEL_DIR%\mpv
 set OUTPUT_DIR=%TOP_LEVEL_DIR%\output
 set PORTABLE_EXE_DIR=%TOP_LEVEL_DIR%\portable
 
@@ -71,7 +65,7 @@ set USER_CHOICE=
 set /P USER_CHOICE="Choose an action: "
 echo.
 
-for %%z in (1 2 3 4 5 6 10 11 20 31 32 33 34 40 41) do if "%USER_CHOICE%" == "%%z" goto pkgver
+for %%z in (1 2 3 4 5 6 10 11 20) do if "%USER_CHOICE%" == "%%z" goto pkgver
 if "%USER_CHOICE%" == ""  goto superend
 goto reask
 
@@ -124,10 +118,6 @@ if "%USER_CHOICE%" == "3"  goto portable
 if "%USER_CHOICE%" == "4"  goto portable64
 if "%USER_CHOICE%" == "5"  goto nomplayer
 if "%USER_CHOICE%" == "6"  goto nomplayer64
-if "%USER_CHOICE%" == "31"  goto testsymboliclink
-if "%USER_CHOICE%" == "32"  goto testsymboliclink
-if "%USER_CHOICE%" == "33"  goto testsymboliclink
-if "%USER_CHOICE%" == "34"  goto testsymboliclink
 if "%USER_CHOICE%" == "10"  goto nsispkg
 if "%USER_CHOICE%" == "11"  goto portable
 if "%USER_CHOICE%" == "20"  goto portablesfx
@@ -372,198 +362,7 @@ echo Restoring source folder(s) back to its original state....
 
 goto end
 
-:testsymboliclink
-
-set SymbolicTestDir1=SymbolicTestDir1-%RANDOM%
-set SymbolicTestDir2=SymbolicTestDir2-%RANDOM%
-mkdir "%TEMP%\%SymbolicTestDir1%" >NUL 2>&1
-mklink /D "%TEMP%\%SymbolicTestDir2%" "%TEMP%\%SymbolicTestDir1%" >NUL 2>&1 || (
-  echo Unable to create symbolic links. Run the script elevated ^(Right-click -^> Run as administrator^), enable SeCreateSymbolicLinkPrivilege on your account in the Local Security Policy Editor, or support may be missing from your OS.
-  goto end
-)
-
-if not exist %MPV_DIR% (
-  echo MPV directory not found!
-  goto end
-)
-
-if "%USER_CHOICE%" == "31"  goto nsispkgmpv
-if "%USER_CHOICE%" == "32"  goto nsispkgmpv64
-if "%USER_CHOICE%" == "33"  goto portablempv
-if "%USER_CHOICE%" == "34"  goto portablempv64
-goto end
-
-:nsispkgmpv
-echo --- SMPlayer NSIS MPV Package [32-bit] ---
-echo.
-
-ren %SMPLAYER_DIR%\mplayer mplayer.bak
-mklink /D %SMPLAYER_DIR%\mplayer %MPV_DIR%
-
-%MAKENSIS_EXE_PATH% /V3 /DVER_MAJOR=%VER_MAJOR% /DVER_MINOR=%VER_MINOR% /DVER_BUILD=%VER_BUILD%%VER_REV_CMD% %TOP_LEVEL_DIR%\smplayer.nsi
-
-rmdir %SMPLAYER_DIR%\mplayer
-ren %SMPLAYER_DIR%\mplayer.bak mplayer
-
-if not "%USER_CHOICE%" == "40" goto end
-
-:nsispkgmpv64
-echo --- SMPlayer NSIS MPV Package [64-bit] ---
-echo.
-
-ren %SMPLAYER_DIR64%\mplayer mplayer.bak
-mklink /D %SMPLAYER_DIR64%\mplayer %MPV_DIR%
-
-%MAKENSIS_EXE_PATH% /V3 /DVER_MAJOR=%VER_MAJOR% /DVER_MINOR=%VER_MINOR% /DVER_BUILD=%VER_BUILD%%VER_REV_CMD% /DWIN64 %TOP_LEVEL_DIR%\smplayer.nsi
-
-rmdir %SMPLAYER_DIR64%\mplayer
-ren %SMPLAYER_DIR64%\mplayer.bak mplayer
-
-goto end
-
-:portablempv
-echo --- SMPlayer Portable Package [32-bit] ---
-echo.
-
-:: Check for portable exes
-if not exist %PORTABLE_EXE_DIR%\smplayer-portable.exe (
-  echo SMPlayer portable EXE not found!
-  goto end
-)
-
-if not exist %PORTABLE_EXE_DIR%\smtube-portable.exe (
-  echo Warning: SMTube portable EXE not found!
-)
-
-ren %SMPLAYER_DIR% smplayer-mpv-portable-%ALL_PKG_VER%
-set SMPLAYER_PORTABLE_DIR=%TOP_LEVEL_DIR%\smplayer-mpv-portable-%ALL_PKG_VER%
-
-if not exist %TOP_LEVEL_DIR%\smplayer-mpv-portable-%ALL_PKG_VER% (
-  echo Oops! Unable to find renamed directory, make sure no files are opened.
-  goto end
-)
-
-::
-echo Backing up files...
-
-ren %SMPLAYER_PORTABLE_DIR%\smplayer.exe smplayer.bak
-ren %SMPLAYER_PORTABLE_DIR%\smtube.exe smtube.bak
-ren %SMPLAYER_PORTABLE_DIR%\mplayer mplayer.bak
-
-::
-echo Creating screenshots dir...
-
-mkdir %SMPLAYER_PORTABLE_DIR%\screenshots
-
-::
-echo Copying portable .exe...
-
-copy /y %PORTABLE_EXE_DIR%\smplayer-portable.exe %SMPLAYER_PORTABLE_DIR%\smplayer.exe
-copy /y %PORTABLE_EXE_DIR%\smtube-portable.exe %SMPLAYER_PORTABLE_DIR%\smtube.exe
-
-::
-echo Creating symbolic link to MPV...
-mklink /D %SMPLAYER_PORTABLE_DIR%\mplayer %MPV_DIR%
-
-::
-echo Finalizing package...
-7za a -t7z %OUTPUT_DIR%\smplayer-mpv-portable-%ALL_PKG_VER%.7z %SMPLAYER_PORTABLE_DIR% -xr!*.bak* -xr!open-fonts -xr!docs -xr!imageformats -xr!shortcuts -xr!Finding_subtitles.txt -xr!Not_so_obvious_things.txt -xr!Watching_TV.txt -xr!sample.avi -xr!Notes_about_mpv.txt -xr!dvdmenus.txt -xr!mpv64.exe -xr!mpv64.com -mx9 >nul
-
-echo.
-echo Restoring source folder(s) back to its original state...
-echo.
-rem DO NOT use 'rmdir /q /s' to delete directory symbolic links
-rmdir %SMPLAYER_PORTABLE_DIR%\mplayer
-rmdir %SMPLAYER_PORTABLE_DIR%\screenshots
-del %SMPLAYER_PORTABLE_DIR%\smplayer.exe
-del %SMPLAYER_PORTABLE_DIR%\smtube.exe
-ren %SMPLAYER_PORTABLE_DIR%\smplayer.bak smplayer.exe
-ren %SMPLAYER_PORTABLE_DIR%\smtube.bak smtube.exe
-ren %SMPLAYER_PORTABLE_DIR%\mplayer.bak mplayer
-ren %SMPLAYER_PORTABLE_DIR% smplayer-build
-
-if not "%USER_CHOICE%" == "41"  goto end
-
-:portablempv64
-echo --- SMPlayer Portable Package [64-bit] ---
-echo.
-
-:: Check for portable exes
-if not exist %PORTABLE_EXE_DIR%\smplayer-portable64.exe (
-  echo SMPlayer portable EXE not found!
-  goto end
-)
-
-if not exist %PORTABLE_EXE_DIR%\smtube-portable64.exe (
-  echo Warning: SMTube portable EXE not found!
-)
-
-for %%F in (%MPV_DIR%\mpv64.exe %MPV_DIR%\mpv64.com) do if not exist %%F (
-  echo 64-bit MPV executables not found!
-  goto end
-)
-
-ren %SMPLAYER_DIR64% smplayer-mpv-portable-%ALL_PKG_VER%-x64
-set SMPLAYER_PORTABLE_DIR=%TOP_LEVEL_DIR%\smplayer-mpv-portable-%ALL_PKG_VER%-x64
-
-if not exist %TOP_LEVEL_DIR%\smplayer-mpv-portable-%ALL_PKG_VER%-x64 (
-  echo Oops! Unable to find renamed directory, make sure no files are opened.
-  goto end
-)
-
-::
-echo Backing up files...
-
-ren %SMPLAYER_PORTABLE_DIR%\smplayer.exe smplayer.bak
-ren %SMPLAYER_PORTABLE_DIR%\smtube.exe smtube.bak
-ren %SMPLAYER_PORTABLE_DIR%\mplayer mplayer.bak
-
-::
-echo Creating screenshots dir...
-
-mkdir %SMPLAYER_PORTABLE_DIR%\screenshots
-
-::
-echo Copying portable .exe...
-
-copy /y %PORTABLE_EXE_DIR%\smplayer-portable64.exe %SMPLAYER_PORTABLE_DIR%\smplayer.exe
-copy /y %PORTABLE_EXE_DIR%\smtube-portable64.exe %SMPLAYER_PORTABLE_DIR%\smtube.exe
-
-::
-echo Creating symbolic link to MPV...
-mklink /D %SMPLAYER_PORTABLE_DIR%\mplayer %MPV_DIR%
-ren %SMPLAYER_PORTABLE_DIR%\mplayer\mpv.exe mpv.exe.bak32
-ren %SMPLAYER_PORTABLE_DIR%\mplayer\mpv.com mpv.com.bak32
-ren %SMPLAYER_PORTABLE_DIR%\mplayer\mpv64.exe mpv.exe
-ren %SMPLAYER_PORTABLE_DIR%\mplayer\mpv64.com mpv.com
-
-::
-echo Finalizing package...
-7za a -t7z %OUTPUT_DIR%\smplayer-mpv-portable-%ALL_PKG_VER%-x64.7z %SMPLAYER_PORTABLE_DIR% -xr!*.bak* -xr!open-fonts -xr!docs -xr!imageformats -xr!shortcuts -xr!Finding_subtitles.txt -xr!Not_so_obvious_things.txt -xr!Watching_TV.txt -xr!sample.avi -xr!Notes_about_mpv.txt -xr!dvdmenus.txt -mx9 >nul
-
-echo.
-echo Restoring source folder(s) back to its original state...
-echo.
-del %SMPLAYER_PORTABLE_DIR%\smplayer.exe
-del %SMPLAYER_PORTABLE_DIR%\smtube.exe
-ren %SMPLAYER_PORTABLE_DIR%\mplayer\mpv.exe mpv64.exe
-ren %SMPLAYER_PORTABLE_DIR%\mplayer\mpv.com mpv64.com
-ren %SMPLAYER_PORTABLE_DIR%\mplayer\mpv.exe.bak32 mpv.exe
-ren %SMPLAYER_PORTABLE_DIR%\mplayer\mpv.com.bak32 mpv.com
-rem DO NOT use 'rmdir /q /s' to delete directory symbolic links
-rmdir %SMPLAYER_PORTABLE_DIR%\mplayer
-rmdir %SMPLAYER_PORTABLE_DIR%\screenshots
-ren %SMPLAYER_PORTABLE_DIR%\smplayer.bak smplayer.exe
-ren %SMPLAYER_PORTABLE_DIR%\smtube.bak smtube.exe
-ren %SMPLAYER_PORTABLE_DIR%\mplayer.bak mplayer
-ren %SMPLAYER_PORTABLE_DIR% smplayer-build64
-
-goto end
-
 :end
-
-rmdir "%TEMP%\%SymbolicTestDir1%" 2>NUL
-rmdir "%TEMP%\%SymbolicTestDir2%" 2>NUL
 
 pause
 
