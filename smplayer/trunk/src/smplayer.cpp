7105c61fcff3c502c0de8642c1c1d0ba2cf3ac31
redxii1234 <redxii1234@fd4b1137-b938-0410-b384-8fdad0efb714>
2015-01-07 06:00:46 +0000

    Add Windows 8.1 (Qt 4.8.6+/5.2+)

--- smplayer/trunk/src/smplayer.cpp
+++ smplayer/trunk/src/smplayer.cpp
@@ -504,6 +504,9 @@ void SMPlayer::showInfo() {
 		#if QT_VERSION >= 0x040803
 		case QSysInfo::WV_WINDOWS8: win_ver = "Windows 8/Server 2012"; break;
 		#endif
+		#if ((QT_VERSION >= 0x040806 && QT_VERSION < 0x050000) || (QT_VERSION >= 0x050200))
+		case QSysInfo::WV_WINDOWS8_1: win_ver = "Windows 8.1/Server 2012 R2"; break;
+		#endif
 		case QSysInfo::WV_NT_based: win_ver = "NT-based Windows"; break;
 		default: win_ver = QString("Unknown/Unsupported Windows OS"); break;
 	}
