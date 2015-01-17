59be39d049b9c18b30f0138be22d83720777023e
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2015-01-17 22:57:54 +0000

    Use a stringlist for the vf_list

--- src/inforeadermpv.h
+++ src/inforeadermpv.h
@@ -44,8 +44,7 @@ public:
 	InfoList vcList() { return vc_list; };
 	InfoList acList() { return ac_list; };
 #endif
-	InfoList vfList() { return vf_list; };
-
+	QStringList vfList() { return vf_list; };
 	QStringList optionList() { return option_list; };
 
 	int mplayerSVN() { return mplayer_svn; };
@@ -68,8 +67,7 @@ protected:
 	InfoList vc_list;
 	InfoList ac_list;
 #endif
-	InfoList vf_list;
-
+	QStringList vf_list;
 	QStringList option_list;
 
 	int mplayer_svn;
