caca6a7901e22b9771a48df3fcccfcf7850f90bd
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2015-06-02 20:08:27 +0000

    (mpv) Use --framedrop=no when creating the thumbnails

--- smplayer/trunk/src/videopreview/videopreview.cpp
+++ smplayer/trunk/src/videopreview/videopreview.cpp
@@ -277,7 +277,7 @@ bool VideoPreview::runPlayer(int seek, double aspect_ratio) {
 	if (PlayerID::player(mplayer_bin) == PlayerID::MPV) {
 		// MPV
 		args << "--no-config" << "--no-audio" << "--no-cache";
-		args << "--frames=6" << "--start=" + QString::number(seek);
+		args << "--frames=6" << "--framedrop=no" << "--start=" + QString::number(seek);
 		if (aspect_ratio != 0) {
 			args << "--video-aspect=" + QString::number(aspect_ratio);
 		}
