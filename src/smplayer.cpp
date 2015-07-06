659d91f9f3a538f50b017e916ef9248638bc5a93
redxii1234 <redxii1234@fd4b1137-b938-0410-b384-8fdad0efb714>
2015-07-06 04:26:51 +0000

    Update for Qt 5.5/Windows 10

--- src/smplayer.cpp
+++ src/smplayer.cpp
@@ -537,7 +537,7 @@ void SMPlayer::showInfo() {
 		#if ((QT_VERSION >= 0x040806 && QT_VERSION < 0x050000) || (QT_VERSION >= 0x050200))
 		case QSysInfo::WV_WINDOWS8_1: win_ver = "Windows 8.1/Server 2012 R2"; break;
 		#endif
-		#if (QT_VERSION >= 0x040807 && QT_VERSION < 0x050000)
+		#if ((QT_VERSION >= 0x040807 && QT_VERSION < 0x050000) || (QT_VERSION >= 0x050500))
 		case QSysInfo::WV_WINDOWS10: win_ver = "Windows 10"; break;
 		#endif
 		case QSysInfo::WV_NT_based: win_ver = "NT-based Windows"; break;
