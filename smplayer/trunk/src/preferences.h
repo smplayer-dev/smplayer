8f7e0e55c201c37cd0460c3b34f0958b86af2ee4
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2015-10-02 21:19:40 +0000

    Add center_window option for config file

--- smplayer/trunk/src/preferences.h
+++ smplayer/trunk/src/preferences.h
@@ -349,6 +349,7 @@ public:
 #endif
 
 	bool move_when_dragging; //!< Move the main window when the video area is dragged
+	bool center_window; //!< Center the main window when playback starts
 
 	// Function of mouse buttons:
 	QString mouse_left_click_function;
