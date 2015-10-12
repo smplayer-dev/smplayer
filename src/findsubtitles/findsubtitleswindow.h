05bf863fa6550ffb3b1067adb287f47d15147938
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2015-10-12 21:17:15 +0000

    Add an option in the find subtitles configuration window to append or not the language code to the subtitle filename.

--- src/findsubtitles/findsubtitleswindow.h
+++ src/findsubtitles/findsubtitleswindow.h
@@ -16,8 +16,8 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#ifndef _FINDSUBTITLESWINDOW_H_
-#define _FINDSUBTITLESWINDOW_H_
+#ifndef FINDSUBTITLESWINDOW_H
+#define FINDSUBTITLESWINDOW_H
 
 #include "ui_findsubtitleswindow.h"
 
