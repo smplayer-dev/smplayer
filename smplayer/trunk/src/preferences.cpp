83fa0c29b3b29a39e1ca0542144f1eb71182e723
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2016-04-18 22:07:22 +0000

    Set the default size of the floating_control to 100%

--- smplayer/trunk/src/preferences.cpp
+++ smplayer/trunk/src/preferences.cpp
@@ -549,7 +549,7 @@ void Preferences::reset() {
        **************** */
 
 	floating_control_margin = 0;
-	floating_control_width = 70; //70 %
+	floating_control_width = 100; //100%
 	floating_control_animated = true;
 	floating_display_in_compact_mode = false;
 	floating_activation_area = AutohideWidget::Anywhere;
