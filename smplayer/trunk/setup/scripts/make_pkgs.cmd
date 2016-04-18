ea3c660f1989437076218e61a6507acb13294303
redxii1234 <redxii1234@fd4b1137-b938-0410-b384-8fdad0efb714>
2016-04-18 16:45:11 +0000

    Revert r7671

--- smplayer/trunk/setup/scripts/make_pkgs.cmd
+++ smplayer/trunk/setup/scripts/make_pkgs.cmd
@@ -98,6 +98,7 @@ set VER_MINOR=
 set VER_BUILD=
 set VER_REVISION=
 set VER_REV_CMD=
+set DEF_QT5=
 set /p ALL_PKG_VER="Version: "
 echo.
 
@@ -159,8 +160,14 @@ goto end
 echo --- SMPlayer NSIS Package [32-bit] ---
 echo.
 
+if exist %TOP_LEVEL_DIR%\smplayer-build\Qt5Core.dll (
+  set DEF_QT5=/DQT5
+) else (
+  set DEF_QT5=
+)
+
 if exist %TOP_LEVEL_DIR%\smplayer-build (
-  %MAKENSIS_EXE_PATH% /V3 /DVER_MAJOR=%VER_MAJOR% /DVER_MINOR=%VER_MINOR% /DVER_BUILD=%VER_BUILD% %VER_REV_CMD% %TOP_LEVEL_DIR%\smplayer.nsi
+  %MAKENSIS_EXE_PATH% /V3 /DVER_MAJOR=%VER_MAJOR% /DVER_MINOR=%VER_MINOR% /DVER_BUILD=%VER_BUILD% %VER_REV_CMD% %DEF_QT5% %TOP_LEVEL_DIR%\smplayer.nsi
 )
 
 if not "%USER_CHOICE%" == "10"  goto end
@@ -169,8 +176,14 @@ if not "%USER_CHOICE%" == "10"  goto end
 echo --- SMPlayer NSIS Package [64-bit] ---
 echo.
 
+if exist %TOP_LEVEL_DIR%\smplayer-build64\Qt5Core.dll (
+  set DEF_QT5="/DQT5"
+) else (
+  set DEF_QT5=
+)
+
 if exist %TOP_LEVEL_DIR%\smplayer-build64 (
-  %MAKENSIS_EXE_PATH% /V3 /DVER_MAJOR=%VER_MAJOR% /DVER_MINOR=%VER_MINOR% /DVER_BUILD=%VER_BUILD% %VER_REV_CMD% /DWIN64 %TOP_LEVEL_DIR%\smplayer.nsi
+  %MAKENSIS_EXE_PATH% /V3 /DVER_MAJOR=%VER_MAJOR% /DVER_MINOR=%VER_MINOR% /DVER_BUILD=%VER_BUILD% %VER_REV_CMD% /DWIN64 %DEF_QT5% %TOP_LEVEL_DIR%\smplayer.nsi
 )
 
 goto end
@@ -364,15 +377,27 @@ goto end
 
 :portablesfx
 
+if exist %TOP_LEVEL_DIR%\smplayer-build\Qt5Core.dll (
+  set DEF_QT5=/DQT5
+) else (
+  set DEF_QT5=
+)
+
 if exist %PORTABLE_EXE_DIR%\smplayer-portable.exe (
   if exist %TOP_LEVEL_DIR%\smplayer-build (
-  %MAKENSIS_EXE_PATH% /V3 /DVER_MAJOR=%VER_MAJOR% /DVER_MINOR=%VER_MINOR% /DVER_BUILD=%VER_BUILD% %VER_REV_CMD% %TOP_LEVEL_DIR%\smportable.nsi
+  %MAKENSIS_EXE_PATH% /V3 /DVER_MAJOR=%VER_MAJOR% /DVER_MINOR=%VER_MINOR% /DVER_BUILD=%VER_BUILD% %VER_REV_CMD% %DEF_QT5% %TOP_LEVEL_DIR%\smportable.nsi
   )
+)
+
+if exist %TOP_LEVEL_DIR%\smplayer-build64\Qt5Core.dll (
+  set DEF_QT5=/DQT5
+) else (
+  set DEF_QT5=
 )
 
 if exist %PORTABLE_EXE_DIR%\smplayer-portable64.exe (
   if exist %TOP_LEVEL_DIR%\smplayer-build64 (
-  %MAKENSIS_EXE_PATH% /V3 /DVER_MAJOR=%VER_MAJOR% /DVER_MINOR=%VER_MINOR% /DVER_BUILD=%VER_BUILD% %VER_REV_CMD% /DWIN64 %TOP_LEVEL_DIR%\smportable.nsi
+  %MAKENSIS_EXE_PATH% /V3 /DVER_MAJOR=%VER_MAJOR% /DVER_MINOR=%VER_MINOR% /DVER_BUILD=%VER_BUILD% %VER_REV_CMD% /DWIN64 %DEF_QT5% %TOP_LEVEL_DIR%\smportable.nsi
   )
 )
 
