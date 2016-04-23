0aa1b2ed6df601bc4547898066538736c24921b5
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2016-04-23 22:52:55 +0000

    Don't set QT_DEVICE_PIXEL_RATIO if the variable is already set

--- smplayer/trunk/src/main.cpp
+++ smplayer/trunk/src/main.cpp
@@ -24,7 +24,9 @@
 int main( int argc, char ** argv ) 
 {
 #if QT_VERSION >= 0x050400 && QT_VERSION < 0x050600
-	qputenv("QT_DEVICE_PIXEL_RATIO", QByteArray("auto"));
+	if (qgetenv("QT_DEVICE_PIXEL_RATIO").isEmpty()) {
+		qputenv("QT_DEVICE_PIXEL_RATIO", QByteArray("auto"));
+	}
 #elif QT_VERSION >= 0x050600
 	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
