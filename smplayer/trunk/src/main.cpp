799c93c87523df071f2efa205800e95faecb52b6
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2016-05-16 20:50:54 +0000

    Load/save the hdpi config from QStandardPaths::ConfigLocation

--- smplayer/trunk/src/main.cpp
+++ smplayer/trunk/src/main.cpp
@@ -22,14 +22,13 @@
 #include <QDir>
 
 #ifdef HDPI_SUPPORT
-#include "paths.h"
 #include "hdpisupport.h"
 #endif
 
 int main( int argc, char ** argv )
 {
 #ifdef HDPI_SUPPORT
-	HDPISupport * hdpi = new HDPISupport(Paths::configPath());
+	HDPISupport * hdpi = new HDPISupport();
 #endif
 
 	MyApplication a( "smplayer", argc, argv );
