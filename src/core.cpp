7fc4b4081aecccbdc84426bc820c944235858a80
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2016-12-17 22:00:46 +0000

    Add an option to display the bitrate info in the status bar

--- src/core.cpp
+++ src/core.cpp
@@ -3653,11 +3653,13 @@ void Core::gotStartingTime(double time) {
 void Core::gotVideoBitrate(int b) {
 	qDebug("Core::gotVideoBitrate: %d", b);
 	mdat.video_bitrate = b;
+	emit bitrateChanged(mdat.video_bitrate, mdat.audio_bitrate);
 }
 
 void Core::gotAudioBitrate(int b) {
 	qDebug("Core::gotAudioBitrate: %d", b);
 	mdat.audio_bitrate = b;
+	emit bitrateChanged(mdat.video_bitrate, mdat.audio_bitrate);
 }
 
 void Core::changePause() {
