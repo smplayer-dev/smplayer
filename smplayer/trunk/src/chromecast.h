3ec19e661a09799dfdb857ceba21ebd66b3fa64d
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2016-12-26 22:11:48 +0000

    Add chromecast.h/cpp

---
+++ smplayer/trunk/src/chromecast.h
@@ -0,0 +1,39 @@
+/*  smplayer, GUI front-end for mplayer.
+    Copyright (C) 2006-2016 Ricardo Villalba <rvm@users.sourceforge.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifndef CHROMECAST_H
+#define CHROMECAST_H
+
+#include <QString>
+
+class Chromecast {
+
+public:
+	Chromecast();
+	~Chromecast();
+
+	void openStream(const QString & url, const QString & title);
+
+	static Chromecast * instance();
+
+private:
+	static Chromecast * instance_obj;
+};
+
+#endif
+
