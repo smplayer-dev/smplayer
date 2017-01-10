1e4213a55880fe5eae5df60bbf7cad89eaf34fc8
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2017-01-10 18:36:03 +0000

    Delete webserver\simple_web_server.exe

--- clean_windows.cmd
+++ clean_windows.cmd
@@ -9,5 +9,7 @@ del src\translations\smplayer_*.qm
 del src\object_script.smplayer.Release
 del src\object_script.smplayer.Debug
 del src\svn_revision.h
+del webserver\simple_web_server.exe
+
 rd src\release
 rd src\debug
