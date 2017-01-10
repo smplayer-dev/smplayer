80d2d9a745430748f7f0456f4e10ba4e402d62bc
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2017-01-10 18:18:29 +0000

    Compile webserver

--- compile_windows_portable.cmd
+++ compile_windows_portable.cmd
@@ -2,9 +2,11 @@ call getrev.cmd
 
 cd zlib
 mingw32-make -fwin32\makefile.gcc
-cd ..
 
-cd src
+cd ..\webserver
+mingw32-make
+
+cd ..\src
 lrelease smplayer.pro
 qmake DEFINES+=PORTABLE_APP
 mingw32-make
