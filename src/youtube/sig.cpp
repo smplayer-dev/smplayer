ab730a107b1b86f414fa7d4edfcdc9a2efdb79e7
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2017-01-31 21:12:28 +0000

    Fix youtube

--- src/youtube/sig.cpp
+++ src/youtube/sig.cpp
@@ -51,9 +51,11 @@ QString Sig::findFunctions(const QString & text) {
 	qDebug() << "Sig::findFunctions: sts:" << sts;
 
 	QString sig_name;
-	QRegExp rx_sig("\\.sig\\|\\|([a-zA-Z0-9\\$]+)\\(");
+	//QRegExp rx_sig("\\.sig\\|\\|([a-zA-Z0-9\\$]+)\\(");
+	QRegExp rx_sig("([\"\'])signature\\1\\s*,\\s*([a-zA-Z0-9$]+)\\(");
 	if (rx_sig.indexIn(text) != -1) {
-		sig_name = rx_sig.cap(1);
+		//sig_name = rx_sig.cap(1);
+		sig_name = rx_sig.cap(2);
 	}
 	qDebug() << "Sig::findFunctions: sig_name:" << sig_name;
 
