c0a04c298ac40e3eac29be3430a7a2fa3144b01e
redxii1234 <redxii1234@fd4b1137-b938-0410-b384-8fdad0efb714>
2017-07-11 11:01:37 +0000

    Read #define DEVELOPMENT_VERSION

--- getrev2.cmd
+++ getrev2.cmd
@@ -18,20 +18,23 @@ echo.
 
 echo #define SVN_REVISION "%svn_revision%">src\svn_revision.h
 
-:: Get values of USE_SVN_VERSIONS & VERSION
+:: Get values of USE_SVN_VERSIONS & DEVELOPMENT_VERSION & VERSION
 for /f "tokens=3" %%j in ('type src\version.cpp ^| find /I "#define USE_SVN_VERSIONS"') do set use_svn_versions=%%j
-for /f "tokens=3" %%k in ('type src\version.cpp ^| find /I "#define VERSION"') do set version_cpp=%%k
+for /f "tokens=3" %%k in ('type src\version.cpp ^| find /I "#define DEVELOPMENT_VERSION"') do set development_version=%%k
+for /f "tokens=3" %%l in ('type src\version.cpp ^| find /I "#define VERSION"') do set version_cpp=%%l
 
 :: Remove quotes
 setlocal enableDelayedExpansion
 for /f "delims=" %%A in ("!use_svn_versions!") do endlocal & set "use_svn_versions=%%~A"
 setlocal enableDelayedExpansion
+for /f "delims=" %%A in ("!development_version!") do endlocal & set "development_version=%%~A"
+setlocal enableDelayedExpansion
 for /f "delims=" %%A in ("!version_cpp!") do endlocal & set "version_cpp=%%~A"
 
 :: Verify svn revision & version are actually numbers and version has at least ver_major.ver_minor.ver_build
-if defined use_svn_versions (
+if defined development_version (
   echo %svn_revision%|findstr /r /c:"^[0-9][0-9]*$" >nul
-  if errorlevel 1 (set use_svn_versions=)
+  if errorlevel 1 (set development_version=)
 )
 
 if defined version_cpp (
@@ -40,7 +43,7 @@ if defined version_cpp (
 )
 
 if defined version_cpp (
-  if [%use_svn_versions%]==[1] (
+  if [%development_version%]==[1] (
     echo %version_cpp%.%svn_revision%>setup\scripts\pkg_version
   ) else (
     echo %version_cpp%>setup\scripts\pkg_version
@@ -48,5 +51,6 @@ if defined version_cpp (
 )
 
 set svn_revision=
+set development_version=
 set use_svn_versions=
 set version_cpp=
