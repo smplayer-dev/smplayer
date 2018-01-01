4cab5478d1318afc7a31f647dda02e8d6b7e285b
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2018-01-01 21:56:50 +0000

    Update copyright year

--- smplayer/trunk/src/myslider.cpp
+++ smplayer/trunk/src/myslider.cpp
@@ -1,5 +1,5 @@
 /*  smplayer, GUI front-end for mplayer.
-    Copyright (C) 2006-2017 Ricardo Villalba <rvm@users.sourceforge.net>
+    Copyright (C) 2006-2018 Ricardo Villalba <rvm@users.sourceforge.net>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
