ee81fccfb2f393b67ff2b738814a2070fdf21c1a
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2018-05-20 19:30:12 +0000

    Rename debian dir (2)

--- smplayer/trunk/create_deb.sh
+++ smplayer/trunk/create_deb.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
  
-ln -s debian-rvm debian
-cp debian-rvm/changelog-orig debian-rvm/changelog
+ln -s debian-orig debian
+cp debian-orig/changelog-orig debian-orig/changelog
 
 ./get_svn_revision.sh
 dch -v 18.4.0-`cat svn_revision` "New version"
@@ -15,5 +15,5 @@ fakeroot debian/rules build
 fakeroot debian/rules binary
 
 dh_clean
-rm debian-rvm/changelog
+rm debian-orig/changelog
 rm debian
