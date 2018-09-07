202a71fac7722ee928f842b2af769a080785e339
rvm3000 <rvm3000@fd4b1137-b938-0410-b384-8fdad0efb714>
2018-09-07 17:31:13 +0000

    Reduce the size of the tray icon

--- smplayer/trunk/src/chromecast.cpp
+++ smplayer/trunk/src/chromecast.cpp
@@ -87,7 +87,7 @@ Chromecast::Chromecast(QObject * parent)
 
 #ifdef CC_USE_TRAY_ICON
 	tray_icon = new QSystemTrayIcon(parent);
-	tray_icon->setIcon(Images::icon("cast"));
+	tray_icon->setIcon(Images::icon("cast", 22));
 	tray_icon->setToolTip(tr("SMPlayer web server is running"));
 	tray_menu = new QMenu;
 	QAction * stopServerAct = new QAction(Images::icon("close"), tr("&Stop SMPlayer server"), tray_menu);
