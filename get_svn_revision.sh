c35bda97b12ace70a259703b7ba731616a3e7aa2
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2021-04-09 16:14:08 +0200

    Simplify get_svn_revision.sh

--- get_svn_revision.sh
+++ get_svn_revision.sh
@@ -6,26 +6,20 @@
 
 test "$1" && extra="-$1"
 
-PREFIX="SVN"
 svn_revision=`LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2`
 test $svn_revision || svn_revision=`cd "$1" && LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2`
 test $svn_revision || svn_revision=`cd "$1" && grep revision .svn/entries 2>/dev/null | cut -d '"' -f2`
 test $svn_revision || svn_revision=`cd "$1" && sed -n -e '/^dir$/{n;p;q}' .svn/entries 2>/dev/null`
+test $svn_revision || svn_revision=`git rev-list --count HEAD`
 test $svn_revision || svn_revision=0UNKNOWN
 
-if test "$svn_revision" = "0UNKNOWN"; then
-    git_revision=`git rev-list --count HEAD`
-    if [ -n "$git_revision" ]; then
-        PREFIX="GIT"
-        svn_revision=$git_revision
-    fi
+if test "$2"; then
+extra="-$2"
 fi
-
 NEW_REVISION="#define SVN_REVISION \"${svn_revision}${extra}\""
 OLD_REVISION=`cat src/svn_revision.h 2> /dev/null`
 
 # Update version.h only on revision changes to avoid spurious rebuilds
 if test "$NEW_REVISION" != "$OLD_REVISION"; then
     echo "$NEW_REVISION" > src/svn_revision.h
-    echo "$PREFIX-r${svn_revision}${extra}" > svn_revision
 fi
