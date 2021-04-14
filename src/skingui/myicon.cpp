3b6355797e94d95a09f8dd666b3792fa4f2c4bc3
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2021-04-14 16:34:22 +0200

    Fix a compile warning in myicon.cpp

--- src/skingui/myicon.cpp
+++ src/skingui/myicon.cpp
@@ -30,17 +30,17 @@ void MyIcon::setPixmap(QPixmap pix, Mode mode, State state )
     switch(mode)
     {
     case Normal:
-        if(state == Off) normalPixOff = pix;
-        else normalPixOn = pix; break;
+        if(state == Off) normalPixOff = pix; else normalPixOn = pix;
+		break;
     case MouseOver:
-        if(state == Off) mouseOverPixOff = pix;
-        else mouseOverPixOn = pix; break;
+        if(state == Off) mouseOverPixOff = pix; else mouseOverPixOn = pix;
+		break;
     case MouseDown:
-        if(state == Off) mouseDownPixOff = pix;
-        else mouseDownPixOn = pix; break;
+        if(state == Off) mouseDownPixOff = pix; else mouseDownPixOn = pix;
+		break;
     case Disabled:
-        if(state == Off) disabledPixOff = pix;
-        else disabledPixOn = pix;break;
+        if(state == Off) disabledPixOff = pix; else disabledPixOn = pix;
+		break;
     }
 }
 
@@ -54,8 +54,7 @@ QPixmap MyIcon::pixmap(Mode mode, State state) const
     QPixmap p = actualPixmap(mode, state);
     if(p.isNull())
     {
-        if(state == Off) return normalPixOff;
-        else return normalPixOn;
+        if(state == Off) return normalPixOff; else return normalPixOn;
     }
     else return p;
 }
@@ -65,17 +64,13 @@ QPixmap MyIcon::actualPixmap(Mode mode, State state ) const
     switch(mode)
     {
     case Normal:
-        if(state == Off) return normalPixOff;
-        else return normalPixOn;
+        if(state == Off) return normalPixOff; else return normalPixOn;
     case MouseOver:
-        if(state == Off) return mouseOverPixOff;
-        else return mouseOverPixOn;
+        if(state == Off) return mouseOverPixOff; else return mouseOverPixOn;
     case MouseDown:
-        if(state == Off) return mouseDownPixOff;
-        else return mouseDownPixOn;
+        if(state == Off) return mouseDownPixOff; else return mouseDownPixOn;
     case Disabled:
-        if(state == Off) return disabledPixOff;
-        else return disabledPixOn;
+        if(state == Off) return disabledPixOff; else return disabledPixOn;
     }
     return QPixmap();
 }
