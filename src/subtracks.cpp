877a2a9f13d1264f5442867d2fde9362c1791a47
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2021-04-14 17:28:06 +0200

    Fix a compile warning in subtracks.cpp

--- src/subtracks.cpp
+++ src/subtracks.cpp
@@ -43,7 +43,7 @@ void SubTracks::add( SubData::Type t, int ID ) {
 }
 
 void SubTracks::list() {
-	for (unsigned int n=0; n < subs.count(); n++) {
+	for (int n=0; n < subs.count(); n++) {
 		qDebug("SubTracks::list: item %d: type: %d ID: %d lang: '%s' name: '%s' filename: '%s'",
                n, subs[n].type(), subs[n].ID(), subs[n].lang().toUtf8().data(),
                subs[n].name().toUtf8().data(), subs[n].filename().toUtf8().data() );
@@ -51,7 +51,7 @@ void SubTracks::list() {
 }
 
 void SubTracks::listNames() {
-	for (unsigned int n=0; n < subs.count(); n++) {
+	for (int n=0; n < subs.count(); n++) {
 		qDebug("SubTracks::list: item %d: '%s'",
                n, subs[n].displayName().toUtf8().data() );
 	}
@@ -115,7 +115,7 @@ int SubTracks::selectOne(QString preferred_lang, int default_sub) {
 #endif
 
 int SubTracks::find( SubData::Type t, int ID ) {
-	for (unsigned int n=0; n < subs.count(); n++) {
+	for (int n=0; n < subs.count(); n++) {
 		if ( ( subs[n].type() == t ) && ( subs[n].ID() == ID ) ) {
 			return n;
 		}
