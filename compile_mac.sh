35daf4e2975f1887d1930151ef4341c77dc7c110
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2021-05-20 14:56:41 +0200

    Rename smplayer.app to SMPlayer.app

--- compile_mac.sh
+++ compile_mac.sh
@@ -2,9 +2,10 @@
 ./get_svn_revision.sh
 cd webserver && make
 cd ../src
-qmake && make
+qmake DEFINES+=NO_DEBUG_ON_CONSOLE && make -j4
 lrelease smplayer.pro
 mkdir smplayer.app/Contents/MacOS/translations
 cp translations/*.qm smplayer.app/Contents/MacOS/translations/
 cp ../webserver/simple_web_server smplayer.app/Contents/MacOS/
-macdeployqt smplayer.app -dmg
+mv smplayer.app SMPlayer.app
+macdeployqt SMPlayer.app -dmg
