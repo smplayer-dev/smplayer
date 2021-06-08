174b55fc1cca2f15b04c7d379cbb12d4a624a111
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2021-06-08 16:12:20 +0200

    Update compile_mac.sh

--- compile_mac.sh
+++ compile_mac.sh
@@ -2,10 +2,13 @@
 ./get_svn_revision.sh
 cd webserver && make
 cd ../src
-qmake DEFINES+=NO_DEBUG_ON_CONSOLE && make -j4
+qmake DEFINES+="NO_DEBUG_ON_CONSOLE USE_SHM USE_GL_WINDOW"
+make -j4
 lrelease smplayer.pro
 mkdir smplayer.app/Contents/MacOS/translations
 cp translations/*.qm smplayer.app/Contents/MacOS/translations/
 cp ../webserver/simple_web_server smplayer.app/Contents/MacOS/
-mv smplayer.app SMPlayer.app
-macdeployqt SMPlayer.app -dmg
+cd ..
+mkdir package
+mv src/smplayer.app package/SMPlayer.app
+macdeployqt package/SMPlayer.app
