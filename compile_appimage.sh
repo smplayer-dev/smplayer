9556e2ee0cb6e78b7ca995467e0afab56ef6dde5
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2021-07-14 11:29:45 +0200

    Check for USE_SMTUBE_LIB

--- compile_appimage.sh
+++ compile_appimage.sh
@@ -5,10 +5,25 @@ echo Compiling web server
 cd webserver && make
 cd ..
 
+SM_DEFINES="NO_DEBUG_ON_CONSOLE USE_SHM USE_GL_WINDOW"
+
+echo $USE_SMTUBE_LIB
+if [ -n "$USE_SMTUBE_LIB" ]; then
+  SM_DEFINES="${SM_DEFINES} SMTUBE_LIB"
+
+  echo Compiling smtube
+  ln -sf ../smtube/src smtube
+  cd smtube
+  lrelease smtube.pro
+  qmake DEFINES+="NO_DEBUG_ON_CONSOLE SMTUBE_LIB"
+  make -j4
+  cd ..
+fi
+
 echo Compiling smplayer
 cd src
 lrelease smplayer.pro
-qmake DEFINES+="NO_DEBUG_ON_CONSOLE USE_SHM USE_GL_WINDOW"
+qmake DEFINES+="$SM_DEFINES"
 make -j4
 cd ..
 
@@ -21,22 +36,9 @@ cp src/translations/*.qm appdir/usr/bin/translations/
 mkdir -p appdir/usr/share/applications
 cp smplayer.desktop appdir/usr/share/applications/
 
-install -d appdir/usr/share/icons/hicolor/512x512/apps/
-install -d appdir/usr/share/icons/hicolor/256x256/apps/
-install -d appdir/usr/share/icons/hicolor/192x192/apps/
-install -d appdir/usr/share/icons/hicolor/128x128/apps/
-install -d appdir/usr/share/icons/hicolor/64x64/apps/
-install -d appdir/usr/share/icons/hicolor/32x32/apps/
-install -d appdir/usr/share/icons/hicolor/22x22/apps/
-install -d appdir/usr/share/icons/hicolor/16x16/apps/
 install -d appdir/usr/share/icons/hicolor/scalable/apps/
-install -m 644 icons/smplayer_icon512.png appdir/usr/share/icons/hicolor//512x512/apps/smplayer.png
-install -m 644 icons/smplayer_icon256.png appdir/usr/share/icons/hicolor/256x256/apps/smplayer.png
-install -m 644 icons/smplayer_icon192.png appdir/usr/share/icons/hicolor/192x192/apps/smplayer.png
-install -m 644 icons/smplayer_icon128.png appdir/usr/share/icons/hicolor/128x128/apps/smplayer.png
-install -m 644 icons/smplayer_icon64.png appdir/usr/share/icons/hicolor/64x64/apps/smplayer.png
-install -m 644 icons/smplayer_icon32.png appdir/usr/share/icons/hicolor/32x32/apps/smplayer.png
-install -m 644 icons/smplayer_icon22.png appdir/usr/share/icons/hicolor/22x22/apps/smplayer.png
-install -m 644 icons/smplayer_icon16.png appdir/usr/share/icons/hicolor/16x16/apps/smplayer.png
 install -m 644 icons/smplayer.svg appdir/usr/share/icons/hicolor/scalable/apps/smplayer.svg
 
+if [ -n "$USE_SMTUBE_LIB" ]; then
+  cp ../smtube/translations/*.qm appdir/usr/bin/translations/
+fi
