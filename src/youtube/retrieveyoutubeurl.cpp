42bccef85e4aa93a4665e30c918d55857a797e77
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2021-07-15 12:14:39 +0200

    Find python on linux only

--- src/youtube/retrieveyoutubeurl.cpp
+++ src/youtube/retrieveyoutubeurl.cpp
@@ -228,7 +228,7 @@ void RetrieveYoutubeUrl::runYtdl(const QString & url) {
 
 	QString app_bin = absoluteFilePath(ytdlBin());
 
-	#ifndef Q_OS_WIN
+	#ifdef Q_OS_LINUX
 	QString python_bin = findExecutable("python3");
 	if (python_bin.isEmpty()) python_bin = findExecutable("python2");
 	if (!python_bin.isEmpty()) {
