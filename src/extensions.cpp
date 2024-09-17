c26fc0c875ac0fa07b7ba6dfffb3a82e77d46b0e
smplayer-dev <10128951+smplayer-dev@users.noreply.github.com>
2024-09-17 14:13:59 +0200

    Add webvtt to the list of subtitle extensions

--- src/extensions.cpp
+++ src/extensions.cpp
@@ -71,7 +71,7 @@ Extensions::Extensions()
 	   << "wv" << "shn" << "opus";
 
 	_subtitles << "srt" << "sub" << "ssa" << "ass" << "idx" << "txt" << "smi"
-               << "rt" << "utf" << "aqt" << "vtt" << "mpsub";
+               << "rt" << "utf" << "aqt" << "vtt" << "webvtt" << "mpsub";
 
 	_playlist << "m3u" << "m3u8" << "pls" << "xspf";
 
