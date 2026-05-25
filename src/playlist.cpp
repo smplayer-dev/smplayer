26d1f2316c8acef433bd4e8cb14a162155a17212
smplayer-dev <10128951+smplayer-dev@users.noreply.github.com>
2026-05-25 15:09:39 +0200

    Save the playlist positions to playlist.ini

--- src/playlist.cpp
+++ src/playlist.cpp
@@ -395,6 +395,7 @@ Playlist::Playlist(QWidget * parent, Qt::WindowFlags f)
 }
 
 Playlist::~Playlist() {
+	savePosition();
 	saveSettings();
 	if (set) delete set;
 
@@ -1451,6 +1452,7 @@ bool Playlist::saveXSPF(const QString & filename) {
 
 
 void Playlist::load() {
+	savePosition();
 	if (maybeSave()) {
 		Extensions e;
 		QString s = MyFileDialog::getOpenFileName(
@@ -1494,6 +1496,7 @@ bool Playlist::saveCurrentPlaylist() {
 
 bool Playlist::save(const QString & filename) {
 	qDebug() << "Playlist::save:" << filename;
+	savePosition();
 
 	QString s = filename;
 
@@ -1630,7 +1633,7 @@ void Playlist::showPopup(const QPoint & pos) {
 }
 
 void Playlist::startPlay() {
-	playItem(0);
+	playItem(loadPosition());
 }
 
 void Playlist::playItem(int n, bool later) {
@@ -2706,6 +2709,32 @@ void Playlist::resort() {
 	int col = listView->horizontalHeader()->sortIndicatorSection();
 	Qt::SortOrder order = listView->horizontalHeader()->sortIndicatorOrder();
 	listView->sortByColumn(col, order);
+}
+
+void Playlist::savePosition() {
+	qDebug() << "Playlist::savePosition:" << playlist_filename;
+	if (set && !playlist_filename.isEmpty() && !isEmpty()) {
+		QString key = playlist_filename.toUtf8().toBase64();
+		int current_position = findCurrentItem();
+		qDebug() << "Playlist::savePosition: key:" << key << "position:" << current_position;
+		set->beginGroup("playlist_positions");
+		set->setValue(key, current_position);
+		set->endGroup();
+	}
+}
+
+int Playlist::loadPosition() {
+	int position = 0;
+	if (set) {
+		set->beginGroup("playlist_positions");
+		QString key = playlist_filename.toUtf8().toBase64();
+		qDebug() << "Playlist::loadPosition: key:" << key;
+		int saved_pos = set->value(key, -1).toInt();
+		set->endGroup();
+		if (saved_pos >= 0 && saved_pos < count()) position = saved_pos;
+	}
+	qDebug() << "Playlist::loadPosition:" << playlist_filename << "position:" << position;
+	return position;
 }
 
 #include "moc_playlist.cpp"
